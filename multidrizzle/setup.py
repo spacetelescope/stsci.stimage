d4f240a29b74c5ced2d6f20cf70cef886662a5c4
chanley <chanley@stsci.edu>
2004-06-29 18:03:44 +0000

    06/29/04 -- CJH -- Initial check-in.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@38 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ multidrizzle/setup.py
@@ -0,0 +1,52 @@
+from distutils.core import setup, Extension
+import numarray
+from numarray.numarrayext import NumarrayExtension
+import sys
+
+if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
+    raise SystemExit, "Python 2.3 or later required to build multidrizzle."
+
+def dolocal():
+    """Adds a command line option --local=<install-dir> which is an abbreviation for
+    'put all of multidrizzle in <install-dir>/multidrizzle'."""
+    if "--help" in sys.argv:
+        print >>sys.stderr
+        print >>sys.stderr, " options:"
+        print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir>"
+    for a in sys.argv:
+        if a.startswith("--local="):
+            dir = a.split("=")[1]
+            sys.argv.extend([
+                "--install-lib="+dir,
+                ])
+            sys.argv.remove(a)
+
+def getExtensions():
+    ext = None                   
+    return ext
+
+
+def dosetup(ext):
+    r = setup(name = "multidrizzle",
+              version = "2.0.0",
+              description = "Compute desired statistics values for numarray objects",
+              author = "Warren Hack, Christopher Hanley, Ivo Busko, Robert Jedrzejewski, and Anton Koekemoer",
+              author_email = "help@stsci.edu",
+              license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
+              platforms = ["Linux","Solaris","Mac OS X"],
+              packages=['multidrizzle'],
+              package_dir={'multidrizzle':'lib'},
+              ext_modules=ext)
+    return r
+
+
+def main():
+    args = sys.argv
+    dolocal()
+    ext = getExtensions()
+    dosetup(ext)
+
+
+if __name__ == "__main__":
+    main()
+
