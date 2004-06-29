95308ff67af66ae03871d4f5ea5ff38fefe172a0
chanley <chanley@stsci.edu>
2004-06-29 18:05:20 +0000

    06/29/04 -- CJH -- Modified package description.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@39 fe389314-cf27-0410-b35b-8c050e845b92

--- multidrizzle/setup.py
+++ multidrizzle/setup.py
@@ -29,7 +29,7 @@ def getExtensions():
 def dosetup(ext):
     r = setup(name = "multidrizzle",
               version = "2.0.0",
-              description = "Compute desired statistics values for numarray objects",
+              description = "Automated process for HST image combination and cosmic-ray rejection",
               author = "Warren Hack, Christopher Hanley, Ivo Busko, Robert Jedrzejewski, and Anton Koekemoer",
               author_email = "help@stsci.edu",
               license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
