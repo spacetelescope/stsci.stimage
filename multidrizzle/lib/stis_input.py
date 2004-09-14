f231b63c16c91a7419c5e0707db1280302e52644
chanley <chanley@stsci.edu>
2004-09-14 16:54:58 +0000

    09/14/04 -- CJH -- The setInstrumentParameters method in the NUVInputImage and FUVInputImage classes were modifed to accept user overloading of the default gain and readnoise values.  A user can now specify non-default gain, readnoise, or both values.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@115 fe389314-cf27-0410-b35b-8c050e845b92

--- multidrizzle/lib/stis_input.py
+++ multidrizzle/lib/stis_input.py
@@ -8,8 +8,9 @@
 #               exposure class by use of a plate scale variable passed in through
 #               the constructor.
 #           Version 0.2.0 08/30/04 -- Added support for NUV and FUV MAMAs.
-
-__version__ = '0.2.0'
+#           Version 0.2.1 09/14/-4 -- Modified the setInstrumentParameters methods
+#                to allow for user input.  -- CJH
+__version__ = '0.2.1'
 
 import pydrizzle
 from pydrizzle import fileutil
@@ -60,7 +61,6 @@ class STISInputImage (InputImage):
             print 'ERROR: invalid instrument task parameter'
             raise ValueError
 
-#        InputImage.setInstrumentParameters(self, instrpars, pri_header)
 
     def updateMDRIZSKY(self,filename=None):
     
@@ -103,6 +103,16 @@ class STISInputImage (InputImage):
         del __handle
         del __sciext
 
+    def _setMAMAchippars(self):
+        self._setDefaultMAMAGain()
+        self._setDefaultMAMAReadnoise()
+     
+    def _setDefaultMAMAGain(self):
+        self._gain = 1
+
+    def _setDefaultMAMAReadnoise(self):
+        self._rdnoise = 0
+
 class CCDInputImage(STISInputImage):
 
     def __init__(self, input, dqname, platescale, memmap=1):
@@ -122,17 +132,63 @@ class NUVInputImage(STISInputImage):
         """ This method overrides the superclass to set default values into
             the parameter dictionary, in case empty entries are provided.
         """
+        if self._isNotValid (instrpars['gain'], instrpars['gnkeyword']):
+            instrpars['gnkeyword'] = None
+        if self._isNotValid (instrpars['rdnoise'], instrpars['rnkeyword']):
+            instrpars['rnkeyword'] = None
         if self._isNotValid (instrpars['exptime'], instrpars['expkeyword']):
             instrpars['expkeyword'] = 'EXPTIME'
         if instrpars['crbit'] == None or instrpars['crbit'] == 0:
             instrpars['crbit'] = self.cr_bits_value
 
-        self._gain      = 1
-        self._rdnoise   = 0
         self._exptime   = self.getInstrParameter(instrpars['exptime'], pri_header,
                                                  instrpars['expkeyword'])
         self._crbit     = instrpars['crbit']
 
+        if self._exptime == None:
+            print 'ERROR: invalid instrument task parameter'
+            raise ValueError
+
+        # We need to treat Read Noise and Gain as a special case since it is 
+        # not populated in the STIS primary header for the MAMAs
+        if (instrpars['rnkeyword'] != None):
+            self._rdnoise   = self.getInstrParameter(instrpars['rdnoise'], pri_header,
+                                                     instrpars['rnkeyword'])                                                 
+        else:
+            self._rdnoise = None
+ 
+        if (instrpars['gnkeyword'] != None):
+            self._gain = self.getInstrParameter(instrpars['gain'], pri_header,
+                                                     instrpars['gnkeyword'])
+        else:
+            self._gain = None
+ 
+        # We need to determine if the user has used the default readnoise/gain value
+        # since if not, they will need to supply a gain/readnoise value as well                
+        usingDefaultGain = False
+        usingDefaultReadnoise = False
+        if (instrpars['gnkeyword'] == None):
+            usingDefaultGain = True
+        if (instrpars['rnkeyword'] == None):
+            usingDefaultReadnoise = True
+
+        # Set the default readnoise or gain values based upon the amount of user input given.
+        
+        # Case 1: User supplied no gain or readnoise information
+        if usingDefaultReadnoise and usingDefaultGain:
+            # Set the default gain and readnoise values
+            self._setMAMAchippars()
+        # Case 2: The user has supplied a value for gain
+        elif usingDefaultReadnoise and not usingDefaultGain:
+            # Set the default readnoise value
+            self._setDefaultReadnoise()
+        # Case 3: The user has supplied a value for readnoise 
+        elif not usingDefaultReadnoise and usingDefaultGain:
+            # Set the default gain value
+            self._setDefaultGain()
+        else:
+            # In this case, the user has specified both a gain and readnoise values.  Just use them as is.
+            pass
 
 class FUVInputImage(STISInputImage):
     def __init__(self, input, dqname, platescale, memmap=1):
@@ -145,13 +201,64 @@ class FUVInputImage(STISInputImage):
         """ This method overrides the superclass to set default values into
             the parameter dictionary, in case empty entries are provided.
         """
+        if self._isNotValid (instrpars['gain'], instrpars['gnkeyword']):
+            instrpars['gnkeyword'] = None
+        if self._isNotValid (instrpars['rdnoise'], instrpars['rnkeyword']):
+            instrpars['rnkeyword'] = None
         if self._isNotValid (instrpars['exptime'], instrpars['expkeyword']):
             instrpars['expkeyword'] = 'EXPTIME'
         if instrpars['crbit'] == None or instrpars['crbit'] == 0:
             instrpars['crbit'] = self.cr_bits_value
 
-        self._gain      = 1
-        self._rdnoise   = 0
         self._exptime   = self.getInstrParameter(instrpars['exptime'], pri_header,
                                                  instrpars['expkeyword'])
         self._crbit     = instrpars['crbit']
+
+        if self._exptime == None:
+            print 'ERROR: invalid instrument task parameter'
+            raise ValueError
+
+        # We need to treat Read Noise and Gain as a special case since it is 
+        # not populated in the STIS primary header for the MAMAs
+        if (instrpars['rnkeyword'] != None):
+            self._rdnoise   = self.getInstrParameter(instrpars['rdnoise'], pri_header,
+                                                     instrpars['rnkeyword'])                                                 
+        else:
+            self._rdnoise = None
+        if (instrpars['gnkeyword'] != None):
+            self._gain = self.getInstrParameter(instrpars['gain'], pri_header,
+                                                     instrpars['gnkeyword'])
+        else:
+            self._gain = None
+ 
+
+        if self._exptime == None:
+            print 'ERROR: invalid instrument task parameter'
+            raise ValueError
+
+        # We need to determine if the user has used the default readnoise/gain value
+        # since if not, they will need to supply a gain/readnoise value as well                
+        usingDefaultGain = False
+        usingDefaultReadnoise = False
+        if (instrpars['gnkeyword'] == None):
+            usingDefaultGain = True
+        if (instrpars['rnkeyword'] == None):
+            usingDefaultReadnoise = True
+
+        # Set the default readnoise or gain values based upon the amount of user input given.
+        
+        # Case 1: User supplied no gain or readnoise information
+        if usingDefaultReadnoise and usingDefaultGain:
+            # Set the default gain and readnoise values
+            self._setMAMAchippars()
+        # Case 2: The user has supplied a value for gain
+        elif usingDefaultReadnoise and not usingDefaultGain:
+            # Set the default readnoise value
+            self._setDefaultReadnoise()
+        # Case 3: The user has supplied a value for readnoise 
+        elif not usingDefaultReadnoise and usingDefaultGain:
+            # Set the default gain value
+            self._setDefaultGain()
+        else:
+            # In this case, the user has specified both a gain and readnoise values.  Just use them as is.
+            pass
