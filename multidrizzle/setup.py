a44244a4a88028057427ff28c95f7df1413023b2
iraf <iraf@stsci.edu>
2004-10-11 17:24:52 +0000

    Changing the version string
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@175 fe389314-cf27-0410-b35b-8c050e845b92

--- multidrizzle/setup.py
+++ multidrizzle/setup.py
@@ -24,7 +24,7 @@ def dolocal():
 def main():
     dolocal()
     setup(name = "multidrizzle",
-              version = "2.0.0",
+              version = "2.3.5",
               description = "Automated process for HST image combination and cosmic-ray rejection",
               author = "Warren Hack, Christopher Hanley, Ivo Busko, Robert Jedrzejewski, and Anton Koekemoer",
               author_email = "help@stsci.edu",
