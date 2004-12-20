72339c20ecfee230df697fc3d9933b9f1437afd7
iraf <iraf@stsci.edu>
2004-12-20 20:47:58 +0000

    Checking in released version of setup
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@230 fe389314-cf27-0410-b35b-8c050e845b92

--- multidrizzle/setup.py
+++ multidrizzle/setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
-import sys
+import sys, os.path, string, shutil
 
 if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'final',0):
     raise SystemExit, "Python 2.3 or later required to build multidrizzle."
+
+ver = sys.version_info
+python_exec = 'python' + str(ver[0]) + '.' + str(ver[1])
+
 
 def dolocal():
     """Adds a command line option --local=<install-dir> which is an abbreviation for
@@ -13,17 +17,41 @@ def dolocal():
         print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir>"
     for a in sys.argv:
         if a.startswith("--local="):
-            dir = a.split("=")[1]
+            dir =  os.path.abspath(a.split("=")[1])
             sys.argv.extend([
                 "--install-lib="+dir,
+                "--install-data="+os.path.join(dir,"multidrizzle")
                 ])
             sys.argv.remove(a)
 
+def getDataDir(args):
+    for a in args:
+        if string.find(a, '--home=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = os.path.join(dir, 'lib/python/multidrizzle')
+        elif string.find(a, '--prefix=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = os.path.join(dir, 'lib', python_exec, 'site-packages/multidrizzle')
+        elif a.startswith('--install-data='):
+            dir = string.split(a, '=')[1]
+            data_dir = dir
+        else:
+            data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages/multidrizzle')
+    return data_dir
 
+def copy_doc(data_dir, args):
+    if 'install' in args:
+        doc_dir = os.path.join(data_dir,'doc')
+        if os.path.exists(doc_dir):
+	    try:
+                shutil.rmtree(doc_dir)
+            except:
+                print "Error removing doc directory\n"
+        #os.mkdir(doc_dir)
+	shutil.copytree('doc', doc_dir)
 
-def main():
-    dolocal()
-    setup(name = "multidrizzle",
+def dosetup():
+    r = setup(name = "multidrizzle",
               version = "2.3.6",
               description = "Automated process for HST image combination and cosmic-ray rejection",
               author = "Warren Hack, Christopher Hanley, Ivo Busko, Robert Jedrzejewski, and Anton Koekemoer",
@@ -32,6 +60,15 @@ def main():
               platforms = ["Linux","Solaris","Mac OS X"],
               packages=['multidrizzle'],
               package_dir={'multidrizzle':'lib'})
+
+    return r
+
+def main():
+    args = sys.argv
+    dolocal()
+    data_dir = getDataDir(args)
+    dosetup()
+    copy_doc(data_dir, args)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,34 +1,108 @@
 #!/usr/bin/env python
 
-import os 
+import os, os.path
 from distutils.core import setup
 from distutils.sysconfig import *
 from distutils.command.install import install
-
+import shutil
 
 py_bin = parse_makefile(get_makefile_filename())['BINDIR']
 ver = sys.version_info
 python_exec = 'python' + str(ver[0])+'.'+str(ver[1])
-params = string.join(sys.argv[2:])
-packages = ['pyraf', 'numdisplay']
+args = sys.argv[2:]
+f2cparams = []
+params = []
+moduleparams = []
+for a in args:
+    if a.startswith('--with-f2c='):
+        f2cparams.append('--with-f2c='+os.path.abspath((string.split(a, '=')[1])))
+        sys.argv.remove(a)
+    elif a.startswith('--local='):
+	f2cparams.append('--local='+os.path.abspath(string.split(a, '=')[1]))
+	params.append('--local='+os.path.abspath(string.split(a, '=')[1]))
+	sys.argv.append('--install-lib=%s' % os.path.abspath((string.split(a,"=")[1])))
+	sys.argv.append('--install-data=%s' % os.path.abspath((string.split(a,"=")[1])))
+
+	sys.argv.remove(a)
+    else:
+	f2cparams.append(a)
+	params.append(a)
+
+f2cpar = string.join(f2cparams)
+par = string.join(params)
+f2cpackages = ['pydrizzle']
+packages = ['pyraf', 'numdisplay', 'imagestats', 'multidrizzle']
 topdir = os.getcwd()
 
-if __name__ == '__main__' :
+def getDataDir(args):
+    for a in args:
+        if string.find(a, '--home=') == 0:
+            dir = os.path.abspath(string.split(a, '=')[1])
+            data_dir = os.path.join(dir, 'lib/python')
+        elif string.find(a, '--prefix=') == 0:
+            dir = os.path.abspath(string.split(a, '=')[1])
+            data_dir = os.path.join(dir, 'lib', python_exec, 'site-packages')
+        elif a.startswith('--install-data='):
+            dir = os.path.abspath(string.split(a, '=')[1])
+            data_dir = dir
+        else:
+            data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages')
+    return data_dir
 
-    setup(
-        name="Numarray, PyFITS, PyRAF",
-        version="1.1",
+def dosetup():
+    r = setup(
+        name="STScI Python Software",
+        version="2.0",
         description="",
         author="Science Software Branch, STScI",
-        author_email="help@stsci.edu",
+        maintainer_email="help@stsci.edu",
         url="http://www.stsci.edu/resources/software_hardware/index_html?category=Data_Analysis",
-        py_modules = ['pyfits', 'readgeis', 'fitsdiff']
+        py_modules = ['pyfits', 'readgeis', 'fitsdiff', 'imageiter', 'irafglob',  'makewcs', 'nimageiter', 'numcombine', 'versioninfo']
         )
     for p in packages:
         os.chdir(topdir)
         os.chdir(p)
-        os.system(python_exec + " setup.py install " + params)
+        os.system(python_exec + " setup.py install " + par)
+    for p in f2cpackages:
+        os.chdir(topdir)
+        os.chdir(p)
+        os.system(python_exec + " setup.py install " + f2cpar)
 
+    return r
+
+
+def copy_doc(data_dir, args):
+    if 'install' in args:
+        doc_dir = os.path.join(data_dir,'doc')
+        if os.path.exists(doc_dir):
+	    try:
+                shutil.rmtree(doc_dir)
+            except:
+                print "Error removing doc directory\n"
+	shutil.copytree('doc', doc_dir)
+
+
+def main():
+    args = sys.argv
+    if '--help' in args:
+	for p in (packages + f2cpackages):
+	    os.chdir(topdir)
+            os.chdir(p)
+            os.system(python_exec + " setup.py --help ")
+    else:
+        data_dir = getDataDir(args)
+
+        dosetup()
+        os.chdir(topdir)
+        copy_doc(data_dir, args)
+
+
+
+if __name__ == '__main__' :
+    main()
+
+
+	
 
 
 
