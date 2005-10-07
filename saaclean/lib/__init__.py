df947fa77fb87e7dd0291342aac21fbb37fc0953
dencheva <dencheva@stsci.edu>
2005-10-07 18:00:52 +0000

    Restructuring saaclean for first publiv release
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@395 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ saaclean/lib/__init__.py
@@ -0,0 +1,26 @@
+"""
+saaclean: Module for estimating and removing persistent CR signal due to a
+          prior SAA passage.
+
+Usage:    Normally used via the STSDAS task saaclean in the nicmos package.
+          To use as pure python, create a params object to override any of
+          the default parameters if desired, then invoke clean:
+          >>> mypars=saaclean.params(thresh=0.23)
+          >>> saaclean.clean('inputfile.fits','outputfile.fits',pars=mypars)
+
+For more information:
+          Additional user information, including parameter definitions and more
+          examples, can be found in the help file for the STSDAS saaclean task,
+          located in nicmos$doc/saaclean.hlp.
+
+          The algorithm and IDL prototype are described in the NICMOS
+          ISR 2003-009, by Bergeron and Dickinson, available through the NICMOS
+          webpage.
+
+Dependencies:
+          numarray v0.6 or higher
+          pyfits v0.6 or higher
+          imagestats v0.2.1
+"""
+from saaclean import * #reveals everything
+__version__="0.5 (15 Sep 2005)"
