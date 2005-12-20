61edc54e5c0739abc36744db530aab1955449e9b
dencheva <dencheva@stsci.edu>
2005-12-20 17:11:29 +0000

    Adding license information
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@433 fe389314-cf27-0410-b35b-8c050e845b92

--- puftcorr/setup.py
+++ puftcorr/setup.py
@@ -17,27 +17,47 @@ def dolocal():
         if a.startswith("--local="):
             dir =  os.path.abspath(a.split("=")[1])
             sys.argv.extend([
-                "--install-lib="+dir
+                "--install-lib="+dir,
+                "--install-data="+os.path.join(dir,"puftcorr")
                 ])
             sys.argv.remove(a)
 
+def getDataDir(args):
+    for a in args:
+        if string.find(a, '--home=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = os.path.join(dir, 'lib/python/puftcorr')
+        elif string.find(a, '--prefix=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = os.path.join(dir, 'lib', python_exec, 'site-packages/puftcorr')
+        elif a.startswith('--install-data='):
+            dir = string.split(a, '=')[1]
+            data_dir = dir
+        else:
+            data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages/puftcorr')
+    return data_dir
 
-def dosetup():
+
+def dosetup(data_dir):
     r = setup(name = "puftcorr",
-              version = "0.1",
+    	      version = "0.1",
               description = "Estimates and removes 'Mr. Staypuft' signal from a NICMOS exposure.",
               author = "Howard Bushouse",
               author_email = "help@stsci.edu",
+	      license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
               platforms = ["Linux","Solaris","Mac OS X"],
               packages=['puftcorr'],
-              package_dir={'puftcorr':'lib'})
+              package_dir={'puftcorr':'lib'},
+	      data_files = [(data_dir,['lib/LICENSE.txt'])])
+		
 
     return r
 
 def main():
     args = sys.argv
     dolocal()
-    dosetup()
+    data_dir = getDataDir(args)
+    dosetup(data_dir)
 
 
 if __name__ == "__main__":
