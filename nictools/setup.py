5df3bcf0a1add70cbdbc50dbe69c4fdfec389660
laidler <laidler@stsci.edu>
2007-12-04 20:49:53 +0000

    Reorganizing puftcorr, rnlincor, saaclean into the nictools package
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@1033 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 10272d5ee66f8f99a807f1be27f9e736ccad4909

--- nictools/setup.py
+++ nictools/setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 import sys, os.path
+from distutils.command.install_data import install_data
 
 if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
-    raise SystemExit, "Python 2.3 or later required to build stistools."
+    raise SystemExit, "Python 2.3 or later required to build nictools."
 
 args = sys.argv[:]
 
@@ -15,6 +16,13 @@ for a in args:
         args.remove(a)
         sys.argv.remove(a)
 
+class smart_install_data(install_data):
+    def run(self):
+        #need to change self.install_dir to the library dir
+        install_cmd = self.get_finalized_command('install')
+        self.install_dir = getattr(install_cmd, 'install_lib')
+        return install_data.run(self)
+
 setup(name = "nictools",
       version = "1.0.0",
       description = "Python Tools for NICMOS Data",
@@ -24,6 +32,8 @@ setup(name = "nictools",
       platforms = ["Linux","Solaris","Mac OS X"],
       packages = ['nictools'],
       package_dir={'nictools':'lib'},
+      cmdclass = {'install_data':smart_install_data},
+      data_files = [('nictools',['lib/SP_LICENSE'])],
       )
 
 
