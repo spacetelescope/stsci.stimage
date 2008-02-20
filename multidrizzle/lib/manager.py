5310bafa4c48ca2251ec2a34bac7c13b9bd66119
hack <hack@stsci.edu>
2008-02-20 21:19:12 +0000

    Changes were made to 'createMedian' in 'manager.py' that compute the buffer size to be used for reading the images section-by-section.  The problem being addressed comes from the last section not having enough rows to accomodate the convolution kernel being applied later. The new buffer size correctly computes the number of rows in the final section given any overlap amount.  The question remains whether or not this will allow very large images to use a buffer size large enough to accomodate the convolution kernel remains to be seen.  WJH
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@1161 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 9975af5761edc1e47c8d63e6c9cebe62788d5b56

--- multidrizzle/lib/manager.py
+++ multidrizzle/lib/manager.py
@@ -736,10 +736,13 @@ class ImageManager:
         _imgrows = _imgarr.shape[0]
         print "_imgrows: ",_imgrows
         _nrows = computeBuffRows(_imgarr)
+        _overlaprows = _nrows - (_overlap+1)
         print "_nrows: ",_nrows
-        _niter = int(_imgrows/_nrows)
+#        _niter = int(_imgrows/_nrows)
+        _niter = 1 + int( (_imgrows - _overlaprows)/_nrows)
         print "_niter: ",_niter
-        _lastrows = _imgrows - (_niter*_nrows) 
+#        _lastrows = _imgrows - (_niter*_nrows) 
+        _lastrows = _imgrows - ((_imgrows - _overlaprows)*(_niter-1) + _overlaprows)
         print "_lastrows: ",_lastrows
         
         # check to see if this buffer size will leave enough rows for
