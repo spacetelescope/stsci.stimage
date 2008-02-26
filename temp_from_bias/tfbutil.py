34e34bc6e14431261517f962acca42c625e62c22
grumm <grumm@stsci.edu>
2008-02-26 19:14:05 +0000

    Now contains routines to set parameters, and default for those parameters.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@1173 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 7da4f2fb4ab1d9806fc25aedb08414c28fae4038

--- temp_from_bias/tfbutil.py
+++ temp_from_bias/tfbutil.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import sys
-import time
 
 # Utility functions and parameters for temp_from_bias
 
@@ -9,8 +8,14 @@ QUIET = 0 # verbosity levels
 VERBOSE = 1
 VERY_VERBOSE = 2   
                                                                                 
-# default value
+# default values
 verbosity = VERBOSE
+hdr_key = "TFBT"
+err_key = "TFBE"
+edit_type = "RAW" 
+nref_par = "/grp/hst/cdbs/nref/"
+noclean = False
+force = None
 
 def all_printMsg( message, level=VERBOSE):
 
@@ -25,7 +30,7 @@ def printMsg( message, level=QUIET):
         sys.stdout.flush()
 
 def setVerbosity( verbosity_level):
-    """Copy verbosity to a variable that is global for this file.                                                              
+    """Copy verbosity to a variable that is global for this file.        
        argument: verbosity_level -  an integer value indicating the level of verbosity
     """
                                                                                 
@@ -37,4 +42,51 @@ def checkVerbosity( level):
 
     return (verbosity >= level)
 
+def setHdr_key( hdr_key_value): 
+    """Copy hdr_key to a variable that is global for this file.        
+       argument: hdr_key -  a string for the keyword name to write
+    """
+                                                                                
+    global hdr_key
+    hdr_key = hdr_key_value
+
+def setErr_key( err_key_value): 
+    """Copy err_key to a variable that is global for this file.        
+       argument: err_key -  a string for the keyword for the error estimate
+    """
+                                                                                
+    global err_key
+    err_key = err_key_value
+
+def setEdit_type_key( edit_type_value): 
+    """Copy edit_type_key to a variable that is global for this file.        
+       argument: edit_type_key -  a string for the keyword name to write
+    """
+                                                                                
+    global edit_type
+    edit_type = edit_type_value
+
+def setNoclean( noclean_value): 
+    """Copy no_clean to a variable that is global for this file.        
+       argument: no_clean - string that is either True or False
+    """
+                                                                                
+    global noclean
+    noclean = noclean_value
+
+def setNref( nref_value): 
+    """Copy nref to a variable that is global for this file.        
+       argument: nref - string for name of directory containing nonlinearity file 
+    """
+                                                                                
+    global nref
+    nref = nref_value
+
+def setForce( force_value ):
+    """Copy force to a variable that is global for this file.        
+       argument: force - string that is either None, Q, B, or S
+    """
+                                                                                
+    global force
+    force = force_value
 
