5303274c442120d3a39192c5fa7f33b4d3b1c998
grumm <grumm@stsci.edu>
2008-03-04 22:21:03 +0000

    Utility functions for 'finesky'
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@1193 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 77abbe951e3db934db21f9e91cd6e345e50f1507

---
+++ nictools/lib/fsutil.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+#
+# Author: Dave Grumm (based on work by Tomas Dahlen and Eddie Bergeron)
+# Program: fsutil.py
+# Purpose: utility functions for makemedmask in 'Finesky'
+# History: 03/04/08 - first version
+
+import sys
+
+__version__ = "0.1 (2008 Mar 4)"
+
+QUIET = 0 # verbosity levels
+VERBOSE = 1
+VERY_VERBOSE = 2   
+                                                                                
+# default values
+verbosity = VERBOSE
+thresh = 0.5 
+medfile = 'PY_Med_image.fits'
+callist = '/hal/data2/dev/nicmos_ped/inlist1.lst'
+
+def all_printMsg( message, level=VERBOSE):
+    """
+    @param message: message to print
+    @type message: string
+    @param level: verbosity level
+    @type level: int
+    """
+    if verbosity >= level:     
+      print message
+      sys.stdout.flush()
+
+def printMsg( message, level=QUIET):
+    """
+    @param message: message to print
+    @type message: string
+    @param level: verbosity level
+    @type level: int
+    """
+    if verbosity >= level:
+        print message
+        sys.stdout.flush()
+
+def setVerbosity( verbosity_level):
+    """ Copy verbosity to a variable that is global for this file.
+    @param verbosity_level: level of verbosity
+    @type verbosity_level: int
+    """                                                                                
+    global verbosity
+    verbosity = verbosity_level
+
+def checkVerbosity( level):
+    """
+    @param level: level of verbosity
+    @type level: int
+    @return: true if verbosity is at least as great as level.
+    @rtype: bool
+    """
+    return (verbosity >= level)
+
+
+def setThresh( thresh_value):
+    """ Copy thresh to a variable that is global for this file.
+    @param thresh_value: level of threshold
+    @type thresh_value: float
+    """
+    global thresh
+    thresh = thresh_value
+            
+def setMedfile( medfile_value):
+    """ Copy medfile to a variable that is global for this file.
+    @param medfile_value: name of output file for masked median image
+    @type medfile_value: string
+    """
+    global medfile
+    medfile = medfile_value
+
+            
+def setCallist( callist_value):
+    """ Copy callist to a variable that is global for this file.
+    @param callist_value: name of file listing cal files
+    @type callist_value: string
+    """
+    global callist
+    callist = callist_value                
