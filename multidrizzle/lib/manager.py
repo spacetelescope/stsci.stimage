c1bf11b003fd16d47eecd7562092ef28aa2725d0
dencheva <dencheva@stsci.edu>
2008-03-11 21:10:52 +0000

    use the full output name that process_input returns
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@1218 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 5efc545d9074e5ffdd493c39e685124f2bfd4b51

--- multidrizzle/lib/manager.py
+++ multidrizzle/lib/manager.py
@@ -346,7 +346,7 @@ class ImageManager:
             if _detector == 3: return NIC3InputImage(input,_dqname,_platescale,memmap=0)
         if _instrument == 'WFC3':
             if _detector == 'UVIS': return UVISInputImage(input,_dqname,_platescale,memmap=0)
-            if _detector == 'IR': return IRInputImage(input,_dqname,_platescale,memmap =0)
+            if _detector == 'IR': return IRInputImage(input,_dqname,_platescale,memmmap =0)
 
         # If a supported instrument is not detected, print the following error message
         # and raise an exception.
@@ -1094,10 +1094,8 @@ class ImageManager:
         
         # Update the BUINT keyword in the DRZ product with the
         # appropriate output UNITS
-        if self.output == "final_drz.fits":
-            outputFileName = self.output
-        else:
-            outputFileName = self.output+"_drz.fits"
+        outputFileName = self.output
+
         outputhdu = fileutil.openImage(outputFileName,mode='update')
         
         if outputhdu['SCI'].header.has_key('BUNIT'):
