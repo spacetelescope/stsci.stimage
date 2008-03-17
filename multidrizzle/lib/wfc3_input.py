8e262f01ad4fd264d51af0181ea2e8bd1a6bff6a
chanley <chanley@stsci.edu>
2008-03-17 18:48:59 +0000

    Partial check-in of new WFC3 support implementation.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@1229 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 4bc5340e74bcbd590a0dce610494132d0181645f

--- multidrizzle/lib/wfc3_input.py
+++ multidrizzle/lib/wfc3_input.py
@@ -157,17 +157,6 @@ class WFC3IRInputImage(IRInputImage):
     def _setDefaultReadnoise(self):
         self._rdnoise = 22.0 # electrons
 
-    def _convert2electrons(self):
-        # Image information
-        __handle = fileutil.openImage(self.name,mode='update',memmap=0)
-        __sciext = fileutil.getExtn(__handle,extn=self.extn)        
-
-        N.multiply(__sciext.data,self.getExpTime(),__sciext.data)        
-        
-        __handle.close()
-        del __handle
-        del __sciext
-
     def updateMDRIZSKY(self,filename=None):
     
         if (filename == None):
@@ -177,53 +166,22 @@ class WFC3IRInputImage(IRInputImage):
             _handle = fileutil.openImage(filename,mode='update',memmap=0)
         except:
             raise IOError, "Unable to open %s for sky level computation"%filename
+
         try:
-        
-            # Get the exposure time for the image.  If the exposure time of the image
-            # is 0, set the MDRIZSKY value to 0.  Otherwise update the MDRIZSKY value
-            # in units of electrons per second.
-            if (self.getExpTime() == 0.0):
-                str =  "*******************************************\n"
-                str += "*                                         *\n"
-                str += "* ERROR: Image EXPTIME = 0.               *\n"
-                str += "* MDRIZSKY header value cannot be         *\n"
-                str += "* converted to units of 'electrons/s'        *\n"
-                str += "* MDRIZSKY will be set to a value of '0'  *\n"
-                str += "*                                         *\n"
-                str =  "*******************************************\n"
-                _handle[0].header['MDRIZSKY'] = 0
-                print str
-            else:
-                # Assume the MDRIZSKY keyword is in the primary header.  Try to update
-                # the header value
-                try:
-                    # We need to handle the updates of the header for data in 
-                    # original units of either counts per second or counts
-                    if (_handle[0].header['UNITCORR'].strip() == 'PERFORM'):
-                        print "Updating MDRIZSKY in %s with %f / %f = %f"%(filename,
-                                self.getSubtractedSky(),
-                                self.getExpTime(),
-                                self.getSubtractedSky() / self.getExpTime()
-                                )
-                        _handle[0].header['MDRIZSKY'] = self.getSubtractedSky() / self.getExpTime()
-                    else:
-                        print "Updating MDRIZSKY in %s with %f"%(filename,
-                                self.getSubtractedSky()
-                                )
-                        _handle[0].header['MDRIZSKY'] = self.getSubtractedSky()
-                        
-                # The MDRIZSKY keyword was not found in the primary header.  Add the
-                # keyword to the header and populate it with the subtracted sky value.
-                except:
-                    print "Cannot find keyword MDRIZSKY in %s to update"%filename
-                    if (_handle[0].header['UNITCORR'].strip() == 'PERFORM'):
-                        print "Adding MDRIZSKY keyword to primary header with value %f"%(self.getSubtractedSky()/self.getExpTime())
-                        _handle[0].header.update('MDRIZSKY',self.getSubtractedSky()/self.getExpTime(), 
-                            comment="Sky value subtracted by Multidrizzle")
-                    else:
-                        print "Adding MDRIZSKY keyword to primary header with value %f"%(self.getSubtractedSky())
-                        _handle[0].header.update('MDRIZSKY',self.getSubtractedSky(), 
-                            comment="Sky value subtracted by Multidrizzle")
-                    
+            # Assume the MDRIZSKY keyword is in the primary header.  Try to update
+            # the header value
+            try:
+                print "Updating MDRIZSKY in %s with %f"%(filename,
+                        self.getSubtractedSky()
+                        )
+                _handle[0].header['MDRIZSKY'] = self.getSubtractedSky()
+
+
+            # The MDRIZSKY keyword was not found in the primary header.  Add the
+            # keyword to the header and populate it with the subtracted sky value.
+            except:
+                print "Adding MDRIZSKY keyword to primary header with value %f"%(self.getSubtractedSky())
+                _handle[0].header.update('MDRIZSKY',self.getSubtractedSky(), 
+                                         comment="Sky value subtracted by Multidrizzle")                
         finally:
             _handle.close()
