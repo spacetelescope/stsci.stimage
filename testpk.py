e86579a8ff5030fd03284cfd2d38cfab246d61eb
sienkiew <sienkiew@stsci.edu>
2008-03-18 19:10:40 +0000

    eliminate tabs
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@1237 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 62f9c0ae949f984fc66fd7903c215dcb8fd53756

--- testpk.py
+++ testpk.py
@@ -5,46 +5,46 @@ import string
 
 
 required_versions = {
-	'calcos':		'1.2',
-	'convolve':		'2.0',
-	'imagestats':		'1.2',
-	'image':		'2.0',
-	'imagestats':		'1.2',
-	'multidrizzle':		'3.1.0',
-	'numpy':		'1.0.4',
-	'numdisplay':		'1.3',
-	'pydrizzle':		'6.1',
-	'pyfits':		'1.3',
-	'nictools.rnlincor':	'0.8',
-	'nictools.puftcorr':	'0.17',
-	'pytools.fileutil':	'1.3.1',
-	'pytools.fitsdiff':	'1.4',
-	'pytools.gfit':		'1.0',
-	'pytools.imageiter':	'0.2',
-	'pytools.irafglob':	'1.0',
-	'pytools.iterfile':	'0.2',
-	'pytools.linefit':	'1.0',
-	'pytools.makewcs':	'0.8.1',
-	'pytools.nimageiter':	'0.6',
-	'pytools.nmpfit':	'0.2',
-	'pytools.numcombine':	'0.4.0',
-	'pytools.parseinput':	'0.1.5',
-	'pytools.readgeis':	'2.0',
-	'pytools.versioninfo':	'0.2.0',
-	'pytools.xyinterp':	'0.1',
-	'pytools.wcsutil':	'1.1.0',
-	'nictools.saaclean':	'1.2',
-	'stistools.mktrace':	'1.1',
-	'stistools.sshift':	'1.4',
-	'stistools.stisnoise':	'5.4',
-	'stistools.wx2d':	'1.1',
-	}
+        'calcos':               '1.2',
+        'convolve':             '2.0',
+        'imagestats':           '1.2',
+        'image':                '2.0',
+        'imagestats':           '1.2',
+        'multidrizzle':         '3.1.0',
+        'numpy':                '1.0.4',
+        'numdisplay':           '1.3',
+        'pydrizzle':            '6.1',
+        'pyfits':               '1.3',
+        'nictools.rnlincor':    '0.8',
+        'nictools.puftcorr':    '0.17',
+        'pytools.fileutil':     '1.3.1',
+        'pytools.fitsdiff':     '1.4',
+        'pytools.gfit':         '1.0',
+        'pytools.imageiter':    '0.2',
+        'pytools.irafglob':     '1.0',
+        'pytools.iterfile':     '0.2',
+        'pytools.linefit':      '1.0',
+        'pytools.makewcs':      '0.8.1',
+        'pytools.nimageiter':   '0.6',
+        'pytools.nmpfit':       '0.2',
+        'pytools.numcombine':   '0.4.0',
+        'pytools.parseinput':   '0.1.5',
+        'pytools.readgeis':     '2.0',
+        'pytools.versioninfo':  '0.2.0',
+        'pytools.xyinterp':     '0.1',
+        'pytools.wcsutil':      '1.1.0',
+        'nictools.saaclean':    '1.2',
+        'stistools.mktrace':    '1.1',
+        'stistools.sshift':     '1.4',
+        'stistools.stisnoise':  '5.4',
+        'stistools.wx2d':       '1.1',
+        }
 
 def testpk():
 
     packages=[ ]
     for x in required_versions :
-	packages.append(x)
+        packages.append(x)
 
     packages.sort()
 
@@ -80,13 +80,13 @@ def testpk():
         print "Package ipython was not found. It is not required but if available can be used with pyraf (pyraf --ipython).\n"
     for p in packages:
         try:
-		exec "import " + p
-		try :
-			ver = eval( p + ".__version__" )
-			installed_packages[p] = ver.split(' ')[0]
-		except :
-			installed_packages[0] = 0
-			install_messages.append("problem with package %s" % p )
+                exec "import " + p
+                try :
+                        ver = eval( p + ".__version__" )
+                        installed_packages[p] = ver.split(' ')[0]
+                except :
+                        installed_packages[0] = 0
+                        install_messages.append("problem with package %s" % p )
         except ImportError:
             installed_packages[p] = 0
             install_messages.append("Package %s is required, but is not installed." % p)
@@ -99,11 +99,11 @@ def testpk():
     if len(install_messages) != 0:
         for m in install_messages:
             print m
-	print pyraf_message
+        print pyraf_message
     else:
         print pyraf_message
         print "All packages were successfully installed.\n"
     
     
 if __name__ == '__main__':
-	testpk()
+        testpk()
