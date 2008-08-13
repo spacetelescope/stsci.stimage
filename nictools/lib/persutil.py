eb56ca5a76a748897136479833b7a7a044ee1b41
grumm <grumm@stsci.edu>
2008-08-13 22:54:08 +0000

    moved parameter retreival functions here
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@6925 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: f7ae7adf270bd40835cb06da61a13f4acb2103bb

--- nictools/lib/persutil.py
+++ nictools/lib/persutil.py
@@ -2,6 +2,8 @@
 
 import sys
 import time
+import pyfits
+from optparse import OptionParser  
 
 # Utility functions and parameters for nic_rem_persist
 
@@ -10,7 +12,8 @@ VERBOSE = 1
 VERY_VERBOSE = 2   
                                                                                 
 # default values
-verbosity = VERBOSE
+verbosity = VERBOSE 
+
 persist_lo = 0.5   # only used for pyraf version
 used_lo = 0.5   # only used for pyraf version
 
@@ -39,3 +42,117 @@ def checkVerbosity( level):
     """Return true if verbosity is at least as great as level."""
 
     return (verbosity >= level)
+
+def getPersist_lo( input_file, options):
+    """
+    @param input_file: input ped file
+    @type iput_file: string
+    @options: array of values to sigma clip
+    @type clip:  Float32
+
+    @return: persist_lo
+    @rtype:  float    
+    """
+
+    if (options.persist_lo == None ):    # get pmodfile from header of input_file, and get bepvallo from pmodfile           
+         fh_infile = pyfits.open( input_file)
+         pmodfile =  fh_infile[0].header.get( "PMODFILE" )
+         fh_pmod = pyfits.open( pmodfile)         
+         persist_lo =  fh_pmod[0].header.get( "BEPVALLO" )
+         fh_infile.close(); fh_pmod.close()
+    else:
+         persist_lo = options.persist_lo
+
+    return persist_lo
+
+
+def getUsed_lo( input_file, options):
+    """
+    @param input_file: input ped file
+    @type iput_file: string
+    @options: array of values to sigma clip
+    @type clip:  Float32
+
+    @return:  used_lo
+    @rtype:  float    
+    """
+    if (options.used_lo == None ):    # get pmodfile from header of input_file, and get bepuselo from pmodfile           
+         fh_infile = pyfits.open( input_file)
+         pmodfile =  fh_infile[0].header.get( "PMODFILE" )
+         fh_pmod = pyfits.open( pmodfile)         
+         used_lo =  fh_pmod[0].header.get( "BEPUSELO" )
+         fh_infile.close(); fh_pmod.close()
+    else:
+         used_lo = options.used_lo
+
+    return used_lo
+             
+
+def getPersist_model( input_file, options):
+    """
+    @param input_file: input ped file
+    @type iput_file: string
+    @options: array of values to sigma clip
+    @type clip:  Float32
+
+    @return: persist_model
+    @rtype: string    
+    """
+
+    if (options.persist_model == None ):    # get pmodfile from header of input_file
+         fh_infile = pyfits.open( input_file)
+         persist_model = fh_infile[0].header.get( "PMODFILE" )
+         fh_infile.close(); 
+    else:
+         persist_model = options.persist_model
+
+    return persist_model
+
+
+def getPersist_mask( input_file, options):
+    """
+    @param input_file: iput ped file
+    @type iput_file: string
+    @options: array of values to sigma clip
+    @type clip:  Float32
+
+    @return: persist_mask
+    @rtype: string    
+    """
+    if (options.persist_mask == None ):    # get pmskfile from header of input_file
+             fh_infile = pyfits.open( input_file)
+             persist_mask = fh_infile[0].header.get( "PMSKFILE" )
+             fh_infile.close(); 
+    else:
+             persist_mask = options.persist_mask
+
+    return persist_mask
+
+def getOptions():
+
+    usage = "usage:  %prog [options] inputfile"
+    parser = OptionParser( usage)
+
+    parser.set_defaults( verbosity = VERBOSE)
+    parser.add_option( "-q", "--quiet", action = "store_const",
+                        const = QUIET, dest = "verbosity",
+                        help = "quiet, print nothing")
+    parser.add_option( "-v", "--verbose", action="store_const",
+                        const = VERY_VERBOSE, dest="verbosity",
+                        help="very verbose, print lots of information")
+    parser.add_option( "-p", "--persist_lo", dest = "persist_lo",
+                        help = "minimum allowed value of the persistence")
+    parser.add_option( "-u", "--used_lo", dest = "used_lo",
+                        help = "minimum allowed value of the fraction of pixels used")
+    parser.add_option( "-d", "--persist_model", dest = "persist_model",
+                        help = "filename containing persistence model")
+    parser.add_option( "-m", "--persist_mask", dest = "persist_mask",
+                        help = "filename containing pixel mask")
+
+    (options, args) = parser.parse_args()
+
+    setVerbosity( options.verbosity)
+    verbosity = options.verbosity
+
+    return options, args, parser
+
