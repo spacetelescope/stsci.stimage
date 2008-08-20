584939639e057df785fa94fbb99484d48ddddcf5
sienkiew <sienkiew@stsci.edu>
2008-08-20 21:07:27 +0000

    setup.py mods - now using new setup.py for stsci_python
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@6958 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 57799a71de1b33dab59ce249958874c91b74580d

--- setup.py
+++ setup.py
@@ -1,231 +1,177 @@
 #!/usr/bin/env python
 
-import os, os.path, sys
-import glob
-from distutils.core import setup
-from distutils.sysconfig import *
-from distutils.command.install_data import install_data
+# BUG : how do we put the svnversion tags in everything ?
 
-# set SVN revision based version information for some packages
-import version
-for path in ['pydrizzle', 'multidrizzle', 'pytools'] :
-    # not practical to distinguish who wants fullInfo and who doesn't
-    version.__set_svn_version__(path=path, fullInfo=True)
-if "versiononly" in sys.argv[:2] :
-    sys.exit(0)
+import os.path
 
-from cfg_pyraf import PYRAF_DATA_FILES, PYRAF_SCRIPTS, PYRAF_EXTENSIONS, PYRAF_CLCACHE
-from cfg_pydrizzle import PYDRIZZLE_EXTENSIONS
-from cfg_imagestats import IMAGESTATS_EXTENSIONS
-from cfg_imagemanip import IMAGEMANIP_EXTENSIONS
-from cfg_calcos import CALCOS_EXTENSIONS
-from cfg_ndimage import NDIMAGE_EXTENSIONS
-from cfg_convolve import CONVOLVE_EXTENSIONS
-from cfg_image import IMAGE_EXTENSIONS
+# list of packages to be installed - we have to populate this 
+# to know what to look for
+all_packages_input = [ 
+    "pytools",
+    "pydrizzle",
+    "convolve",
+    "image",
+    "imagemanip",
+    "imagestats",
+    "multidrizzle",
+    "ndimage",
+    "nictools",
+    "numdisplay",
+    "sample_package",
+    "stistools",
+    "wfpc2tools",
+]
 
+for x in [ "pysynphot", "pyraf", "pyfits" ] :
+    if os.path.isdir(x) :
+        all_packages_input.append(x) 
+    else :
+        print ""
+        print "Optional package",x,"not present"
+        print ""
 
-# PACKAGES is the list of all packages that we want to install.  If you
-# want it, make sure it is listed here and in PACKAGE_DIRS
+## If you are just adding a new package, you don't need to edit anything
+## after this line.
 
-PACKAGES = ['calcos','numdisplay', 'imagestats', 'imagemanip',
-    'multidrizzle', 'pydrizzle', 'pydrizzle.traits102',
-    'pydrizzle.distortion','pytools', 'nictools', 
-    'stistools', 'wfpc2tools','ndimage','convolve','image']
+import pprint
+pp = pprint.PrettyPrinter(indent=8)
+pp = pp.pprint
 
+import sys
+import distutils
 
-# uninstall_packages is a list of packages that we want to remove when we do
-# an install, but that we are not installing.  If an old package has been
-# removed from the distribution, list it here.
-#
-uninstall_packages = [ ] 
+# where are the python files in that package
+all_package_dir = { }
 
-#The normal directory structure is {packagename:packagename/lib.}
-#
-# PACKAGE_DIRS[x] is the relative directory where we can find the source code 
-# for package x
-#
-PACKAGE_DIRS = {}
-for p in PACKAGES:
-    PACKAGE_DIRS[p]="%s/lib"%p
+# native code modules
+all_ext_modules = [ ]
 
-#Exceptions are allowed; put them here.
-PACKAGE_DIRS['pydrizzle.traits102']='pydrizzle/traits102'
-PACKAGE_DIRS['pydrizzle.distortion']='pydrizzle/lib/distortion'
-args = sys.argv[2:]
-
-for a in args :
-    print a
-    if a.startswith('--local='):
-        dir = os.path.abspath(a.split("=")[1])
-        sys.argv.extend([
-                "--install-lib="+dir,
-                "--install-scripts=%s" % os.path.join(dir,"pyraf"),
-                ])
-        sys.argv.remove(a)
-        args.remove(a)
-
-
-class smart_install_data(install_data):
-    def run(self):
-        #need to change self.install_dir to the library dir
-        install_cmd = self.get_finalized_command('install')
-        self.install_dir = getattr(install_cmd, 'install_lib')
-        return install_data.run(self)
-
-
-IMAGESTATS_DATA_DIR = os.path.join('imagestats')
-IMAGESTATS_DATA_FILES = ['imagestats/lib/LICENSE.txt']
-
-IMAGEMANIP_DATA_DIR = os.path.join('imagemanip')
-IMAGEMANIP_DATA_FILES = ['imagemanip/lib/LICENSE.txt']
-
-MULTIDRIZZLE_DATA_DIR = os.path.join('multidrizzle')
-MULTIDRIZZLE_DATA_FILES = ['multidrizzle/lib/LICENSE.txt']
-
-NUMDISPLAY_DATA_DIR = os.path.join('numdisplay')
-NUMDISPLAY_DATA_FILES = ['numdisplay/lib/imtoolrc', 'numdisplay/lib/LICENSE.txt','numdisplay/lib/ichar.dat']
-
-PYDRIZZLE_DATA_DIR = os.path.join('pydrizzle')
-PYDRIZZLE_DATA_FILES = ['pydrizzle/lib/LICENSE.txt']
-
-NICTOOLS_DATA_DIR = os.path.join('nictools')
-NICTOOLS_DATA_FILES = ['nictools/lib/SP_LICENSE']
-
-NDIMAGE_DATA_DIR = os.path.join('ndimage')
-NDIMAGE_DATA_DIR = os.path.join('ndimage/tests')
-NDIMAGE_DATA_FILES = ['ndimage/LICENSE.txt',"ndimage/tests/test_ndimage.py"]
-
-CONVOLVE_DATA_DIR = os.path.join('convolve')
-CONVOLVE_DATA_FILES = ['convolve/lib/LICENSE.txt']
-
-IMAGE_DATA_DIR = os.path.join('image')
-IMAGE_DATA_FILES = ['image/lib/LICENSE.txt']
-
-DATA_FILES = [ (NUMDISPLAY_DATA_DIR, NUMDISPLAY_DATA_FILES),
-               (NICTOOLS_DATA_DIR, NICTOOLS_DATA_FILES),
-               (IMAGESTATS_DATA_DIR, IMAGESTATS_DATA_FILES),
-               (MULTIDRIZZLE_DATA_DIR, MULTIDRIZZLE_DATA_FILES),
-               (PYDRIZZLE_DATA_DIR, PYDRIZZLE_DATA_FILES),
-               (IMAGEMANIP_DATA_DIR, IMAGEMANIP_DATA_FILES),
-               (NDIMAGE_DATA_DIR, NDIMAGE_DATA_FILES),
-               (CONVOLVE_DATA_DIR, CONVOLVE_DATA_FILES),
-               (IMAGE_DATA_DIR, IMAGE_DATA_FILES)
-           ]
-
-EXTENSIONS = PYDRIZZLE_EXTENSIONS + IMAGESTATS_EXTENSIONS + IMAGEMANIP_EXTENSIONS + CALCOS_EXTENSIONS + NDIMAGE_EXTENSIONS + CONVOLVE_EXTENSIONS + IMAGE_EXTENSIONS
-
-SCRIPTS = None
-
-#The following packages are part of the stsci_python module distribution,
-#but reside in their own repositories. This setup.py will look for
-#them before attempting to build them.
-#
-#Only try to build PyRAF if the pyraf directory exists locally
-# and we're not on a windwos platform
-if os.path.exists(os.path.join('pyraf')) and sys.platform != 'win32':
-    PACKAGES.append('pyraf')
-    PACKAGE_DIRS['pyraf']='pyraf/lib'
-    PYRAF_DATA_DIR = os.path.join('pyraf')
-    PYRAF_CLCACHE_DIR = os.path.join('pyraf', 'clcache')
-    EXTENSIONS = EXTENSIONS + PYRAF_EXTENSIONS
-    DATA_FILES.extend([(PYRAF_DATA_DIR, PYRAF_DATA_FILES), (PYRAF_CLCACHE_DIR, PYRAF_CLCACHE)])
-    SCRIPTS = ['pyraf/lib/pyraf']
-
-#Only install pyfits if the pyfits directory exists locally
-if os.path.exists(os.path.join('pyfits')):
-    PACKAGES.append('pyfits')
-    PACKAGE_DIRS['pyfits']='pyfits/lib'
-
-# install pysynphot if the pysynphot directory exists
-if os.path.exists(os.path.join('pysynphot')):
-    PACKAGES.append('pysynphot')
-    PACKAGE_DIRS['pysynphot']='pysynphot/lib'
-    DATA_FILES.append( ( "pysynphot", glob.glob(os.path.join('pysynphot','test','etctest_base_class.py')) ) )
-    PYSYNPHOT_DATA_DIR = os.path.join('pysynphot','data')
-    DATA_FILES.append( ( PYSYNPHOT_DATA_DIR, glob.glob(os.path.join('pysynphot', 'data', 'generic', '*')) ) )
-    DATA_FILES.append( ( PYSYNPHOT_DATA_DIR, glob.glob(os.path.join('pysynphot', 'data', 'wavecat', '*')) ) )
+# scripts to go in a bin/ directory somewhere
+all_scripts = [ ]
 
 #
+all_data_files = [ ]
+
 #
-#
-def uninstall_modules( modules ) :
-    # bug: this function should observe the -q / -v flags
-    print "Begin uninstall"
+all_packages = [ ]
 
-    # To uninstall a module, you need to know what directory it is in.
-    # To find that information, you can import it and look at __path__
-    # That will be a list of things that you can recursively remove to delete the
-    # module.
 
-    # BUT:  If you import something, it finds the module in the current directory.
-    # That is bad, because we have everything here with us. To fix that, purge the 
-    # current directory from sys.path before we start looking for modules.
+# For each package we want to use, fetch the config data.
+# We have to fix various things because each package description
+# thinks it is only talking about itself, but now we are in the
+# parent directory.
 
-    cwd = os.getcwd()
-    l = [ ]
-    for x in sys.path :
-        if x == cwd or x == '' or x == '.' :
-            continue
-        l.append(x)
-    sys.path = l
+for lpkg in all_packages_input :
 
-    # Now we walk the list of modules that we want to uninstall and
-    # remove each one.
+    all_packages.append(lpkg)
 
-    import shutil
+    # forget anything about previous packages that we processed
+    pkg = None
+    setupargs = None
 
-    # I want to make sure that we remove all copies of the module that are
-    # in the search path.  If we don't, we might remove the first one, only
-    # to find another one behind it somewhere.  So, keep trying until we
-    # can't find anything more to delete.  
+
+    # "import" the defsetup file.  We can't use import because
+    # none of these are in modules, but we can read/exec.
     #
-    # If we can't remove one of the modules, we also can't remove anything
-    # that it shadows.  removed_list is a list of everything we tried to
-    # remove; if it is still there in a second pass, we ignore it so we
-    # don't get in to an infinite loop.
+    # We want to protect our current environment from contamination
+    # by the exec'ed code, so we give it a private symbol table
 
-    removed_list = [ ]
-    any_removed = 1
-    while any_removed :
-        any_removed=0
-        for x in modules :
-            try :
-                mod = __import__( x )
-            except :
-                # modules that don't exist are ok
-                continue
+    e_dict = { }
 
-            for dir in mod.__path__ :
-                if not dir in removed_list :
-                    removed_list.append(dir)
-                    try :
-                        shutil.rmtree(dir, 0)
-                    except :
-                        print "    remove",dir,"failed"
-                    any_removed=1
-    print "End uninstall"
+    fname = lpkg+"/defsetup.py"
+    f = open(fname,"r")
+    exec f in e_dict
+    f.close()
 
-if sys.argv[1] == "uninstall" :
-    # Uninstall any old copies of packages that we don't want around.  That is
-    # a list of all the packages we are installing, plus a list of whatever
-    # other things we might be wanting to outdate.  (i.e. was in the last release
-    # but should not be in this one.  It would be better for distutils to have a 
-    # real uninstall, but I am not going to implement one right now.)
-    # I am assuming that "module" and "package" are the same thing, though I am 
-    # not sure that is strictly true.
-    uninstall_modules( PACKAGES + uninstall_packages )
-    sys.exit(0)
+    # Pick out the two interesting variables from the exec'ed code
 
-setup(name="STScI Python Software",
-      version="2.7dev",
-      description="",
-      author="Science Software Branch, STScI",
-      maintainer_email="help@stsci.edu",
-      url="http://www.stsci.edu/resources/software_hardware/index_html?category=Data_Analysis",
-      packages = PACKAGES,
-      package_dir = PACKAGE_DIRS,
-      cmdclass = {'install_data':smart_install_data},
-      data_files = DATA_FILES,
-      scripts = SCRIPTS,
-      ext_modules = EXTENSIONS,
-      )
+    if "pkg" in e_dict :
+        pkg = e_dict["pkg"]
+    if "setupargs" in e_dict :
+        setupargs = e_dict["setupargs"]
+
+    if isinstance(pkg,str) :
+        pkg = [ pkg ]
+
+    # if the package doesn't say that it's package name is what
+    # we thought it was, we have a problem
+
+    if lpkg != pkg[0] :
+        raise "yow! package name doesn't match"
+
+    # pick out the "lib" directory, where the pure python comes from
+    if not 'package_dir' in setupargs :
+        # not specified, use the default
+        all_package_dir[pkg[0]] = "%s/%s" % ( pkg[0], 'lib' )
+        
+    else :
+        # there is a list of package dirs to handle
+        package_dir = setupargs['package_dir']
+        for x in package_dir :
+            if not x in all_packages :
+                all_packages.append(x)
+            all_package_dir[x] = "%s/%s" % (pkg[0], package_dir[x])
+
+    # If there are scripts, we have to correct the file names where
+    # the installer can find them.  Each is under the pkg directory.
+
+    if 'scripts' in setupargs :
+        for x in setupargs['scripts'] :
+            all_scripts.append("%s/%s" % ( pkg[0], x ))
+
+    # If there are external modules, we need to correct the file names
+    # of source files.
+
+    if 'ext_modules' in setupargs :
+        for x in setupargs['ext_modules'] :
+            l = [ ]
+            for y in x.sources :
+                l.append("%s/%s"%(pkg[0],y))
+            x.sources = l
+            all_ext_modules.append(x)
+
+    # If there are data files, we need to correct the file names.
+
+    if 'data_files' in setupargs :
+        for x in setupargs['data_files'] :
+            ( instdir, files ) = x
+            t = [ ]
+            for y in files :
+                t.append( pkg[0] + "/" + y )
+            all_data_files.append( ( instdir, t ) )
+
+
+####
+#
+# Fix distutils to work the way we want it to.
+#
+# we can't just import this because it isn't installed python
+# won't find the module.  So, we just read the file and exec it.
+#
+# This exec exists soley for the modifications it makes to distutils.
+
+f=open("pytools/lib/stsci_distutils_hack.py","r")
+exec f in { }
+f.close()
+
+####
+
+distutils.core.setup(
+
+    # This name is used in various file names to identify this item.
+    name="stsci_python",
+
+    # This version is expected to be compared to other version numbers.
+    # It will also appear in some file names.
+    version="2.7dev",
+
+    # Description is not used.
+    description="",
+
+    packages = all_packages,
+    package_dir = all_package_dir,
+    ext_modules = all_ext_modules,
+    scripts = all_scripts,
+    data_files = all_data_files,
+    
+)
