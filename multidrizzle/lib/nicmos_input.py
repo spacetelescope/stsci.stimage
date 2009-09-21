Checking in my WFC3 and units processing fixes to the trunk Multidrizzle trunk.
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@8298 fe389314-cf27-0410-b35b-8c050e845b92

Former-commit-id: c00f72f7248f1175ff6935142cecc05570d5a76c

multidrizzle/lib/nicmos_input.py
@@ -7,16 +7,15 @@ from pytools import fileutil
 from nictools import readTDD
 import numpy as np
 
-from ir_input import IRInputImage
 from input_image import InputImage
 
 
-class NICMOSInputImage(IRInputImage):
+class NICMOSInputImage(InputImage):
 
     SEPARATOR = '_'
 
     def __init__(self, input,dqname,platescale,memmap=0,proc_unit="native"):
-        IRInputImage.__init__(self,input,dqname,platescale,memmap=0,proc_unit=proc_unit)
+        InputImage.__init__(self,input,dqname,platescale,memmap=0,proc_unit=proc_unit)
         
         # define the cosmic ray bits value to use in the dq array
         self.cr_bits_value = 4096
@@ -24,11 +23,14 @@ class NICMOSInputImage(IRInputImage):
         # Detector parameters
         self.platescale = platescale
         self.full_shape = (256,256)
-         
-        # no cte correction for NICMOS so set cte_dir=0.
-        self.cte_dir = 0   
+        self.native_units = "COUNTS/S"
 
+        # no cte correction for NICMOS so set cte_dir=0.
+        self.cte_dir = 0
+        
+        # All data is converted to electrons so use effective gain of 1
         self._effGain = 1
+        
 
     def updateMDRIZSKY(self,filename=None): 
         if (filename == None): 
@@ -57,18 +59,15 @@ class NICMOSInputImage(IRInputImage):
             if (_handle[0].header['UNITCORR'].strip() == 'PERFORM'): 
                 skyvalue = self.getSubtractedSky()/self.getExpTime() 
             else: 
-                skyvalue = self.getSubtractedSky() 
+                skyvalue = self.getSubtractedSky()
             # We need to convert back to native units if computations were done in electrons
             if self.proc_unit != "native":
-                skyvalue = skyvalue/self.getGain()
+                skyvalue = skyvalue/self.getGain()    
             print "Updating MDRIZSKY keyword to primary header with value %f"%(skyvalue) 
             _handle[0].header.update('MDRIZSKY',skyvalue)  
         _handle.close() 
 
     def doUnitConversions(self): 
-        """ Converts data units to a count image, from count rate image. 
-            If proc_unit == 'electrons', the gain correction will be applied.
-        """
         # Image information        
         _handle = fileutil.openImage(self.name,mode='update',memmap=0) 
         _sciext = fileutil.getExtn(_handle,extn=self.extn)         
@@ -76,39 +75,28 @@ class NICMOSInputImage(IRInputImage):
         # Determine if Multidrizzle is in units of counts/second or counts 
         # 
         # Counts per second case 
-        exptime = self.getExpTime()  
-        if (_sciext.header['BUNIT'].find('/') > -1):         
+        if (_handle[0].header['UNITCORR'].strip() == 'PERFORM'):         
             # Multiply the values of the sci extension pixels by the gain. 
-            print "Converting %s from COUNTS/S to "%(self.name), 
+            print "Converting %s from COUNTS/S to ELECTRONS"%(self.name) 
             # If the exptime is 0 the science image will be zeroed out. 
-            conversionFactor = exptime
-            
+            conversionFactor = (self.getExpTime() * self.getGain())
+
         # Counts case 
         else:
             # Multiply the values of the sci extension pixels by the gain. 
-            print "Converting %s from COUNTS to "%(self.name), 
+            print "Converting %s from COUNTS to ELECTRONS"%(self.name) 
             # If the exptime is 0 the science image will be zeroed out. 
-            conversionFactor = 1.0
-            
-        # Implement conversion to electrons if specified by user
-        if self.proc_unit == "electrons":
-            print 'ELECTRONS'
-            conversionFactor *= self.getGain()
-            self._expscale = self.getGain()
+            conversionFactor = (self.getGain())  
 
-            # Set the BUNIT keyword to 'electrons'
-            bunit_val = _sciext.header['BUNIT']
-            _sciext.header.update('BUNIT',bunit_val.replace('COUNTS','ELECTRONS'))
-
-            # Update the PHOTFLAM value
-            photflam = _handle[0].header['PHOTFLAM']
-            _handle[0].header.update('PHOTFLAM',(photflam/self.getGain()))
-        else:
-            print 'COUNTS'
+        np.multiply(_sciext.data,conversionFactor,_sciext.data)
         
-        if conversionFactor != 1.0:   
-            np.multiply(_sciext.data,conversionFactor,_sciext.data)
-                    
+        # Set the BUNIT keyword to 'electrons'
+        _handle[0].header.update('BUNIT','ELECTRONS')
+
+        # Update the PHOTFLAM value
+        photflam = _handle[0].header['PHOTFLAM']
+        _handle[0].header.update('PHOTFLAM',(photflam/self.getGain()))
+        
         # Close the files and clean-up
         _handle.close() 
 
@@ -165,8 +153,8 @@ class NICMOSInputImage(IRInputImage):
         
         # Convert the science data to electrons if specified by the user.  Each
         # instrument class will need to define its own version of doUnitConversions
-        #if self.proc_unit == "electrons":
-        self.doUnitConversions()        
+        if self.proc_unit == "electrons":
+            self.doUnitConversions()
 
     def _setchippars(self):
         self._setDefaultReadnoise()
@@ -274,13 +262,9 @@ class NIC1InputImage(NICMOSInputImage):
         
     def _setDarkRate(self):
         self.darkrate = 0.08 #electrons/s
-        if self.proc_unit == 'native':
-            self.darkrate = self.darkrate / self.getGain() # DN/s
 
     def _setDefaultReadnoise(self):
         self._rdnoise = 26.0 # electrons
-        if self.proc_unit == 'native':
-            self._rdnoise = self._rdnoise / self.getGain() # ADU
 
 class NIC2InputImage(NICMOSInputImage):
     def __init__(self, input, dqname, platescale, memmap=0,proc_unit="native"):
@@ -289,13 +273,9 @@ class NIC2InputImage(NICMOSInputImage):
         
     def _setDarkRate(self):
         self.darkrate = 0.08 #electrons/s
-        if self.proc_unit == 'native':
-            self.darkrate = self.darkrate / self.getGain() # DN/s
 
     def _setDefaultReadnoise(self):
         self._rdnoise = 26.0 #electrons
-        if self.proc_unit == 'native':
-            self._rdnoise = self._rdnoise/self.getGain() #ADU
 
 class NIC3InputImage(NICMOSInputImage):
     def __init__(self, input, dqname, platescale, memmap=0,proc_unit="native"):
@@ -304,10 +284,6 @@ class NIC3InputImage(NICMOSInputImage):
         
     def _setDarkRate(self):
         self.darkrate = 0.15 #electrons/s
-        if self.proc_unit == 'native':
-            self.darkrate = self.darkrate/self.getGain() #DN/s
 
     def _setDefaultReadnoise(self):
         self._rdnoise = 29.0 # electrons
-        if self.proc_unit == 'native':
-            self._rdnoise = self._rdnoise/self.getGain() #ADU

multidrizzle/lib/wfc3_input.py
@@ -4,9 +4,8 @@
 #   Purpose: Class used to model WFC3 specific instrument data.
 
 from pytools import fileutil
-import numpy as n
+import numpy as np
 from input_image import InputImage
-from ir_input import IRInputImage
 
 class WFC3UVISInputImage(InputImage):
 
@@ -17,8 +16,7 @@ class WFC3UVISInputImage(InputImage):
 
         # define the cosmic ray bits value to use in the dq array
         self.cr_bits_value = 4096
-        self.platescale = platescale
-        
+                
         # Effective gain to be used in the driz_cr step.  Since the
         # WFC3 UVIS images have already been converted to electrons,
         # the effective gain is 1.
@@ -27,6 +25,7 @@ class WFC3UVISInputImage(InputImage):
         self.instrument = 'WFC3/UVIS'
         self.full_shape = (4096,2048)
         self.platescale = platescale
+        self.native_units = "electrons"
 
         # get direction, which depends on which chip but is independent of amp 
         if ( self.extn == 'sci,1') : 
@@ -73,8 +72,7 @@ class WFC3UVISInputImage(InputImage):
 
         # Convert the science data to electrons if specified by the user.  Each
         # instrument class will need to define its own version of doUnitConversions
-        if self.proc_unit == "electrons":
-            self.doUnitConversions()
+        self.doUnitConversions()
 
     def getflat(self):
         """
@@ -147,10 +145,10 @@ class WFC3UVISInputImage(InputImage):
         
         return darkcurrent
 
-class WFC3IRInputImage(IRInputImage):
+class WFC3IRInputImage(InputImage):
 
     def __init__(self, input, dqname, platescale, memmap=0,proc_unit="native"):
-        IRInputImage.__init__(self,input,dqname,platescale,memmap=0,proc_unit=proc_unit)
+        InputImage.__init__(self,input,dqname,platescale,memmap=0,proc_unit=proc_unit)
         
         # define the cosmic ray bits value to use in the dq array
         self.cr_bits_value = 4096
@@ -166,21 +164,19 @@ class WFC3IRInputImage(IRInputImage):
         self.instrument = 'WFC3/IR'
         self.full_shape = (1000,1000)
         self.platescale = platescale
+        self.native_units = "ELECTRONS/S"
 
     def doUnitConversions(self):
-        """ Convert units to a count image from a count rate image.
-        """
         # Image information 
         _handle = fileutil.openImage(self.name,mode='update',memmap=0) 
         _sciext = fileutil.getExtn(_handle,extn=self.extn)         
 
-        # Check to see if conversion to count-rate image was turned on in calibration
-        if _sciext.header['BUNIT'].find('/') > -1:
-            # Set the BUNIT keyword to 'electrons'
-            _handle[1].header.update('BUNIT','ELECTRONS')
-            print "Converting %s from ELECTRONS/S to ELECTRONS"%(self.name)
-            n.multiply(_sciext.data,self.getExpTime(),_sciext.data)            
-                    
+        # Set the BUNIT keyword to 'electrons'
+        _handle[1].header.update('BUNIT','ELECTRONS')
+
+        # Counts case 
+        np.multiply(_sciext.data,self.getExpTime(),_sciext.data)
+        
         # Close the files and clean-up
         _handle.close() 
 
@@ -206,12 +202,10 @@ class WFC3IRInputImage(IRInputImage):
             _handle[0].header['MDRIZSKY'] = 0 
             print str 
         else:
-            # Assume the MDRIZSKY keyword is in the primary header.  Try to update 
-            # the header value
-            if self.proc_unit == 'electrons': 
-                skyvalue = self.getSubtractedSky()/self.getExpTime() 
-            else: 
-                skyvalue = self.getSubtractedSky() 
+            skyvalue = self.getSubtractedSky()
+            if self.proc_unit == 'electrons':
+                skyvalue = skyvalue / self.getExpTime()
+
             print "Updating MDRIZSKY keyword to primary header with value %f"%(skyvalue) 
             _handle[0].header.update('MDRIZSKY',skyvalue)  
         _handle.close() 
@@ -241,10 +235,6 @@ class WFC3IRInputImage(IRInputImage):
             print 'ERROR: invalid instrument task parameter'
             raise ValueError
 
-        # Convert the science data to electrons if specified by the user.  Each
-        # instrument class will need to define its own version of doUnitConversions
-        # always need to convert from electrons/sec to electrons
-        #if self.proc_unit:# == "electrons":
         self.doUnitConversions()
 
     def getflat(self):