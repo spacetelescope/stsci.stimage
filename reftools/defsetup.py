3f07551d65b4dc97297ff74e52179399c6cadd5e
hack <hack@stsci.edu>
2010-02-02 15:16:12 +0000

    A new package has been added to STScI_Python to manage code related to reference file generation and usage, or any other instrument-independent calibration-related code. WJH
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@8699 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 0c8be3c34081ecdbe3b3f9a36c2d578964127b9c

---
+++ reftools/defsetup.py
@@ -0,0 +1,16 @@
+from __future__ import division # confidence high
+
+pkg = "reftools"
+
+setupargs = {
+
+    'version' :         '1.0',
+    'description' :     "Reference File Python Tools",
+    'author' :          "Warren Hack, Nadezhda Dencheva",
+    'author_email' :    "help@stsci.edu",
+    'license' :         "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
+    'platforms' :       ["Linux","Solaris","Mac OS X","Win"],
+    'scripts' :         [ 'lib/update_npol'] ,
+                    
+    }
+
