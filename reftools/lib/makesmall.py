e2bc486180b62f7266c1f3339596c4f6825ee8fb
hack <hack@stsci.edu>
2010-02-02 15:23:38 +0000

    A new package has been added to STScI_Python to manage code related to reference file generation and usage, or any other instrument-independent calibration-related code. The modules originally copied from development/trunk/testtools branch r8698 were: makesmall, test_small_dgeo, and update_npol. WJH
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@8700 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 2592b6c7f4663d62e10e056aca17d6779595cb0e

---
+++ reftools/lib/makesmall.py
@@ -0,0 +1,184 @@
+import pyfits
+import string
+import os
+
+from pytools import fileutil,parseinput
+import numpy as np
+
+__version__ = '0.1.1'
+__vdate__ = '2009-12-22'
+    
+def dxy(dgeofile, filter=None, colcorr=None,corrext='DX',minsize=32,debug=False):
+    ''' Build subsampled CDBS _dxy files from full-frame(Jay's) DXM and DYM images. 
+        
+        If there is a column/row correction provided, it will be removed 
+        before resampling the full DXM,DYM images. In that case, the
+        'corrext' parameter specifies the extension from the full-sized 
+        image that needs to have this column/row correction removed.
+    '''
+     
+    wheel1 = ['F475W', 'F555W', 'F606W', 'F850LP', 'F625W',
+              'F658N', 'F775W', 'F502N', 'F550M']
+    wheel2 = ['F435W', 'F814W', 'F660N']
+    
+    odgeofile = dgeofile
+    # Interpret input filename, expanding any variables used for path to file
+    dgeofile = fileutil.osfn(dgeofile)
+    if not os.path.exists(dgeofile):
+        print' Input: ',dgeofile
+        raise InputError, "No valid input found! "
+
+    # If column/row correction image is specified, open it for removal from
+    # DGEOFILE...
+    if colcorr is not None:
+        if not os.path.exists(colcorr):
+            raise InputError,'No valid column/row correction image found!'
+        
+        # User specified a column/row correction image to be subtracted from
+        # full DGEOFILE before resampling
+        corrimg = pyfits.open(colcorr,'readonly')
+    else:
+        print "======"
+        print 'No column/row correction removed from DGEOFILE before resampling.'
+        print "======"
+
+    # Open file for processing
+    #dxyfile = pyfits.open('qbu16424j_dxy.fits','readonly')         # Model _DXY file
+    dxyfile = pyfits.open(dgeofile,'readonly')         # Model _DXY file
+    detector = dxyfile[0].header['detector']
+    print 'DETECTOR', detector
+    print
+
+    # Read in filter name from DGEOFILE directly, if not specified by user
+    filter1 = None
+    filter2 = None
+    if filter is None:
+        if dxyfile[0].header.has_key('FILTER1'):
+            filter1 = dxyfile[0].header['FILTER1']
+            filter2 = dxyfile[0].header['FILTER2']
+            if filter2 == 'N/A': filter2 = 'CLEAR2S'
+            if filter2.find('CLEAR') > -1: filter = filter1
+            else: filter = filter2
+        else:
+            # Error case, filter name not provided in file or by user 
+            dxyfile.close()
+            print 'ERROR: Filter name not found in DGEOFILE! Please specify...'
+            raise InputError
+        
+    filter = string.upper(filter)     # On input, case can be upper or lower   
+    print 'Filter', filter    
+            
+    # Get the shape of each chip from the first DGEOFILE array
+    dxy_shape = dxyfile[1].data.shape
+        
+    #grid=[4095,2047,64,64]
+    # compute step size needed to create DXY file no smaller than 32x32
+    stepsize = min(dxy_shape[1]/minsize, dxy_shape[0]/minsize)
+    grid=[dxy_shape[1]-1,dxy_shape[0]-1,stepsize,stepsize]
+    xpts = np.array(range(0,grid[0],grid[2]),np.float32)
+    ypts = np.array(range(0,grid[1],grid[3]),np.float32)
+    xygrid = np.meshgrid(xpts,ypts)
+    
+    # count the number of chips in DGEOFILE 
+    numchips = 0
+    extname = dxyfile[1].header['EXTNAME']
+    
+    for extn in dxyfile:
+        if extn.header.has_key('extname') and extn.header['extname'] == extname:
+            numchips += 1
+
+    # process each chip
+    for chip in range(1,numchips+1):
+        # Process DX and DY for each chip
+        for xy in ['DX','DY']:
+            print 'Processing chip from extension: ',xy,',',chip
+            onaxis1 = dxyfile[xy,chip].header['NAXIS1']
+            onaxis2 = dxyfile[xy,chip].header['NAXIS2']
+            if not dxyfile[xy,chip].header.has_key('CCDCHIP'):
+                ccdchip = 1
+            else:
+                ccdchip = dxyfile[xy,chip].header['CCDCHIP']
+            
+            dxy = dxyfile[xy,chip].data.copy()
+            if colcorr is not None and xy == corrext:
+                # Remove column/row correction from this extension
+                xycorr = corrimg[0].data
+            else:
+                xycorr = None
+                
+            # define default chipname for debug results; None indicates no debugging
+            cname = None
+            if debug:
+                cname = 'full_68corr_'+xy+str(chip)+'_dxy.fits'
+
+            dxyfile[xy,chip].data = resample_chip(dxy,xygrid,corrxy=xycorr,chipname=cname)
+            dxyfile[xy,chip].header.update('ONAXIS1', onaxis1, "NAXIS1 of full size dgeo file")
+            dxyfile[xy,chip].header.update('ONAXIS2', onaxis2, "NAXIS2 of full size dgeo file")
+            dxyfile[xy,chip].header.update('CCDCHIP', ccdchip, "CCDCHIP from full size dgeo file")
+    # Get filter info ready for use in updating output image header
+
+    if filter1 is None:
+        if filter in wheel1:
+           filter1 = filter
+           filter2 = 'CLEAR2L'
+           
+        if filter in wheel2:
+           filter2 = filter
+           filter1 = 'CLEAR1L'    
+
+    print filter1, filter2
+    
+    # Update keywords
+    newname = detector.lower()+'_'+str(stepsize)+'_' + string.lower(filter) + '_npl.fits'
+    if os.path.exists(newname): os.remove(newname)
+    dxyfile[0].header['filename'] = newname
+    dxyfile[0].header['filter1'] = filter1
+    dxyfile[0].header['filter2'] = filter2
+    dxyfile[0].header.update('pedigree','INFLIGHT 01/03/2002 01/10/2005')
+    dxyfile.writeto(newname)    
+
+    # close open file handles
+    if colcorr is not None:
+        corrimg.close()
+    dxyfile.close()
+    
+    # finish by cleaning up output image header by removing unnecessary keywords
+    print 'Cleaning up header'
+    fixheader(filter,newname, odgeofile)
+    print 'Finished creating new file: ',newname
+
+def resample_chip(chip,mesh,corrxy=None,chipname=None):
+    """ Apply resampling operations to a single chip's array.
+    If provided, also remove column/row correction from array prior to resampling. 
+    """
+    if corrxy is not None:
+        print 'Removing column correction ',corrxy.shape,' from full chip dxy array...'
+        chip -= corrxy
+    if chipname is not None:
+        if os.path.exists(chipname): os.remove(chipname)
+        outimg = pyfits.PrimaryHDU(data=chip)
+        outimg.writeto(chipname)
+
+    return chip[mesh[1].astype(np.int), mesh[0].astype(np.int)]
+
+def fixheader(filter,filename, oldname):
+    ''' Clean up dxy headers'''
+    required_keys = ['INSTRUME','DETECTOR','FILTER1','FILTER2','COMMENT','HISTORY','PROPOSID','CAL_VER']
+    dxyfile = pyfits.open(filename, mode='update')
+    dxyfile.info()
+    h0 = dxyfile[0].header
+    # remove extraneous keywords from original DGEOFILE
+    for h0key in h0.keys()[15:]:
+        if h0key not in required_keys: del h0[h0key]
+    del h0['']
+    #cards = h0.ascardlist()
+    #del cards[67:]
+    #del cards[32:65]
+    #del cards[14:28]   # Discard most of the header
+    #h0.update('PEDIGREE', 'INFLIGHT 01/03/2002 01/10/2005')
+    h0.update('FILETYPE', 'DXY GRID')
+    h0.update('COMMENT', 'Accuracy to 0.01 pixels when dxy corrections included')
+    h0.add_history('Improved solution as reported in 2005 Cal Workshop')
+    h0.add_history('Average of 64x64 blocks from full DXY image %s' % oldname)
+
+    dxyfile.close()
---
+++ reftools/lib/test_small_dgeo.py
@@ -0,0 +1,331 @@
+#!/usr/bin/env python
+import  pyfits, pywcs
+from pywcs import WCS, DistortionLookupTable
+import numpy as np
+from matplotlib import pyplot as pl
+#pl.hold(False)
+from stwcs import wcsutil,updatewcs
+import numdisplay
+from pytools import fileutil
+import wtraxyutils
+import os
+import imagestats
+
+__version__ = '0.1'
+__vdate__ = '2009-10-07'
+
+#import gc  #call gc.collect() occasionally
+
+def help():
+    print run.__doc__
+
+def build_grid_arrays(nx,ny,step):
+    grid = [nx,ny,step]
+    print 'grid of : ',nx,'x',ny,' by ',step
+        
+    #grid=[4096,2048,1]
+    xpts = np.array(range(1,grid[0]+1,grid[2]),np.float32)
+    ypts = np.array(range(1,grid[1]+1,grid[2]),np.float32)
+    xygrid = np.meshgrid(xpts,ypts)
+    xarr = xygrid[0].flatten()
+    yarr = xygrid[1].flatten()
+
+    return xarr,yarr
+
+def transform_d2im_dgeo(img,extver,xarr,yarr,verbose=False):
+
+    print 'setting up WCS object for ',img,'[sci,',str(extver),']'
+
+    w = wcsutil.HSTWCS(img, ext=('sci',extver))
+    d2imx,d2imy = w.det2im(xarr,yarr,1)
+    if verbose:
+        print 'Det2im results span: '
+        print d2imx.min(),d2imx.max(),d2imx.shape
+        print d2imy.min(),d2imy.max(),d2imy.shape
+    
+    xout, yout = w.p4_pix2foc(d2imx,d2imy,1)
+    if verbose:
+        print 'pix2foc results span: '
+        print xout.min(),xout.max(),xout.shape
+        print yout.min(),yout.max(),yout.shape
+
+    return xout,yout
+
+def run(scifile,dgeofile=None,output=False,match_sci=False,update=True):
+    """ 
+        This routine compares how well the sub-sampled DGEOFILE (generated 
+        using the 'makesmall' module) corrects the input science image as 
+        opposed to the full-size DGEOFILE.  
+        
+        SYNTAX:
+            import test_small_dgeo
+            test_small_dgeo.run(scifile,dgeofile=None,output=False)
+        
+        where:
+            scifile   - name of science image
+            dgeofile  - name of full-sized DGEOFILE if not in ODGEOFILE keyword
+            output    - if True, write out differences to FITS file(s)
+
+        The user can either specify the full-size DGEOFILE reference filename
+        as the 'dgeofile' parameter or the code will look for the 'ODGEOFIL' 
+        keyword in the primary header for the name of the full-sized reference
+        file.  
+        
+        The primary output will be a series of plots showing the difference images
+        with the mean and stddev of the differences in the label of the image display.
+        
+        If the 'output' parameter is set to True, these differences 
+        will then be written out to FITS files based on the input science image
+        rootname. Both the DX and DY differences for a single chip will be written
+        out to the same file, with a separate file for each chip. 
+        
+    """
+    if update:
+        # update input SCI file to be consistent with reference files in header
+        print 'Updating input file ',scifile,' to be consistent with reference files listed in header...'
+        updatewcs.updatewcs(scifile)
+
+    if dgeofile is None:
+        # read in full dgeofile from header
+        fulldgeofile = pyfits.getval(scifile,'ODGEOFIL')
+    else:
+        fulldgeofile = dgeofile
+        
+    print 'Opening full-size DGEOFILE ',fulldgeofile,' for comparison.'
+    fulldgeofile = fileutil.osfn(fulldgeofile)
+    full_shape = [pyfits.getval(fulldgeofile,'NAXIS2','DX',1),pyfits.getval(fulldgeofile,'NAXIS1','DX',1)]
+    
+    filter_names = fileutil.getFilterNames(pyfits.getheader(scifile))
+
+    detector = pyfits.getval(fulldgeofile,'DETECTOR')
+    # count the number of chips in DGEOFILE 
+    xyfile = pyfits.open(scifile)
+    numchips = 0
+    ccdchip = []
+    extname = xyfile[1].header['EXTNAME']
+    for extn in xyfile:
+        if extn.header.has_key('extname') and extn.header['extname'] == extname:
+            numchips += 1
+            if extn.header.has_key('ccdchip'):
+                ccdchip.append(extn.header['ccdchip'])
+            else:
+                ccdchip.append(1)
+    if not match_sci:
+        ltv1 = 0
+        ltv2 = 0  
+        nx = full_shape[1]
+        ny = full_shape[0]
+    else:
+        nx = xyfile['sci',1].header['NAXIS1']
+        ny = xyfile['sci',1].header['NAXIS2']
+        ltv1 = xyfile['sci',1].header['ltv1']
+        ltv2 = xyfile['sci',1].header['ltv2']
+
+    grid = [nx,ny,1]
+    print 'grid of : ',nx,ny
+    xyfile.close()
+
+    xarr,yarr = build_grid_arrays(nx,ny,1)
+    xgarr = xarr.reshape(grid[1],grid[0])
+    ygarr = yarr.reshape(grid[1],grid[0])
+
+    # initialize plot here
+    pl.clf()
+    pl.gray()
+    
+    for chip,det in zip(range(1,numchips+1),ccdchip):
+
+        xout,yout = transform_d2im_dgeo(scifile,chip,xarr,yarr)
+        
+        dgeochip = 1
+        dgeo = pyfits.open(fulldgeofile)
+        for e in dgeo:
+            if not e.header.has_key('ccdchip'):
+                continue
+            else:
+                if e.header['ccdchip'] == det:
+                    dgeochip = e.header['extver']
+                    break
+        dgeo.close()
+        
+        print 'Matching sci,',chip,' with DX,',dgeochip
+        dx= (xout-xarr).reshape(grid[1],grid[0])
+        fulldatax = pyfits.getdata(fulldgeofile,'DX',dgeochip)
+        diffx=(dx-fulldatax[-ltv2:-ltv2+ny,-ltv1:-ltv1+nx]).astype(np.float32)
+
+        pl.imshow(diffx)
+        pl.title('dx-full_x: %s %s(DX,%d) with %g +/- %g'%(filter_names,detector,dgeochip,diffx.mean(),diffx.std()))
+        pl.colorbar()
+        
+        raw_input("Press 'ENTER' to close figure and plot DY...")
+        pl.close()
+
+        dy= (yout-yarr).reshape(grid[1],grid[0])
+        fulldatay = pyfits.getdata(fulldgeofile,'DY',dgeochip)
+        diffy=(dy-fulldatay[-ltv2:-ltv2+ny,-ltv1:-ltv1+nx]).astype(np.float32)
+
+        pl.imshow(diffy)
+        pl.title('dy-full_y: %s %s(DY,%d) with %g +/- %g '%(filter_names,detector,dgeochip,diffy.mean(),diffy.std()))
+        pl.colorbar()
+
+        raw_input("Press 'ENTER' to close figure and show next chip...")
+        pl.close()
+        if output:
+            # parse out rootname from input file if user wants results written to file
+            outroot = fileutil.buildNewRootname(scifile)
+            #
+            # setup DGEOFILE ref file as template for each chip's output results
+            # we only need dx,1 and dy,1 since each chip will be written out
+            # to a separate file and since we will use this template for 
+            # writing out 2 different results files
+            #
+            fhdulist = pyfits.open(fulldgeofile)
+            hdulist = pyfits.HDUList()
+            hdulist.append(fhdulist[0])
+            hdulist.append(fhdulist['dx',1])
+            hdulist.append(fhdulist['dy',1])
+            fhdulist.close()
+            
+            outname = outroot+'_sci'+'_dgeo_diffxy.match'
+            if os.path.exists(outname): os.remove(outname)
+            wtraxyutils.write_xy_file(outname,[xgarr[::32,::32].flatten(),
+                                                ygarr[::32,::32].flatten(),
+                                                (xgarr+diffx)[::32,::32].flatten(),
+                                                (ygarr+diffy)[::32,::32].flatten()],append=True)
+            
+            outname = outroot+'_sci'+str(chip)+'_newfull_dxy.fits'
+            if os.path.exists(outname): os.remove(outname)
+
+            hdulist['dx',1].data = dx
+            hdulist['dy',1].data = dy
+            hdulist.writeto(outname)
+            
+            outname = outroot+'_sci'+str(chip)+'_diff_dxy.fits'
+            if os.path.exists(outname): os.remove(outname)
+            hdulist['dx',1].data = diffx
+            hdulist['dy',1].data = diffy
+            hdulist.writeto(outname)
+            print 'Created output file with differences named: ',outname
+
+        del dx,dy,diffx,diffy
+            
+    if output:
+        hdulist.close()
+        
+def compare_sub_to_full_sci(subarray,full_sci,output=False,update=True):
+    if update:
+        # update input SCI file to be consistent with reference files in header
+        print 'Updating input file ',subarray,' to be consistent with reference files listed in header...'
+        updatewcs.updatewcs(subarray)
+        print 'Updating input file ',full_sci,' to be consistent with reference files listed in header...'
+        updatewcs.updatewcs(full_sci)
+
+    fulldgeofile = fileutil.osfn(pyfits.getval(subarray,'ODGEOFIL'))
+    # parse out rootname from input file if user wants results written to file
+    if output:
+        soutroot = fileutil.buildNewRootname(subarray)
+        foutroot = fileutil.buildNewRootname(full_sci)
+        hdulist = pyfits.open(fulldgeofile)
+
+    detector = pyfits.getval(fulldgeofile,'DETECTOR')
+    filter_names = fileutil.getFilterNames(pyfits.getheader(subarray))
+
+    # count the number of chips in subarray image 
+    xyfile = pyfits.open(subarray)
+    numchips = 0
+    ccdchip = []
+    extname = xyfile[1].header['EXTNAME']
+    for extn in xyfile:
+        if extn.header.has_key('extname') and extn.header['extname'] == extname:
+            numchips += 1
+            if extn.header.has_key('ccdchip'):
+                ccdchip.append([extn.header['ccdchip'],extn.header['extver']])
+            else:
+                ccdchip.append([1,1])
+
+    snx = xyfile['sci',1].header['NAXIS1']
+    sny = xyfile['sci',1].header['NAXIS2']
+    ltv1 = xyfile['sci',1].header['ltv1']
+    ltv2 = xyfile['sci',1].header['ltv2']
+    xyfile.close()
+
+    # build grid of points for full-size image for 
+    #    chips corresponding to subarray 
+    xyfile = pyfits.open(full_sci)
+    fullchip = []
+    for extn in xyfile:
+        if (extn.header.has_key('extname') and extn.header['extname'] == extname) and \
+        extn.header['ccdchip'] == ccdchip[0][0]:
+            fullchip.append([extn.header['ccdchip'],extn.header['extver']])
+    xyfile.close()
+
+    sxarr,syarr = build_grid_arrays(snx,sny,1)
+    full_range = [slice(-ltv2,-ltv2+sny),slice(-ltv1,-ltv1+snx)]
+
+
+    fnx = pyfits.getval(full_sci,'NAXIS1','sci',1)
+    fny = pyfits.getval(full_sci,'NAXIS2','sci',1)
+    fxarr,fyarr = build_grid_arrays(fnx,fny,1)
+    
+    # initialize plot here
+    pl.clf()
+    pl.gray()
+    
+    for chip,det,fext in zip(range(1,numchips+1),ccdchip,fullchip):
+        # Compute the correction imposed by the D2IM+DGEO corrections 
+        #   on the subarray
+        sxout,syout = transform_d2im_dgeo(subarray,det[1],sxarr,syarr)
+        sdx= (sxout-sxarr).reshape(sny,snx)
+        sdy= (syout-syarr).reshape(sny,snx)
+        # Compute the correction imposed by the D2IM+DGEO corrections 
+        #    on the full sized SCI image
+        fxout,fyout = transform_d2im_dgeo(full_sci,fext[1],fxarr,fyarr)
+        fdx= (fxout-fxarr).reshape(fny,fnx)
+        fdy= (fyout-fyarr).reshape(fny,fnx)
+
+        # determine the difference
+        diffx = (sdx - fdx[full_range[0],full_range[1]]).astype(np.float32)
+        pl.imshow(diffx)
+        pl.title('sub_dx-full_x: %s %s[%d:%d,%d:%d] with %g +/- %g'%
+            (filter_names,detector,full_range[0].start,full_range[0].stop,
+             full_range[1].start,full_range[1].stop,
+             diffx.mean(),diffx.std()))
+        pl.colorbar()
+        
+        raw_input("Press 'ENTER' to close figure and plot DY...")
+        pl.close()
+        
+        # determine the difference
+        diffy = (sdy - fdy[full_range[0],full_range[1]]).astype(np.float32)
+        pl.imshow(diffy)
+        pl.title('sub_dy-full_y: %s %s[%d:%d,%d:%d] with %g +/- %g'%
+            (filter_names,detector,full_range[0].start,full_range[0].stop,
+             full_range[1].start,full_range[1].stop,
+             diffy.mean(),diffy.std()))
+        pl.colorbar()
+        
+        raw_input("Press 'ENTER' to close figure and exit...")
+        pl.close()
+        
+        if output:
+            outname = foutroot+'_sci'+str(chip)+'_newfull_dxy.fits'
+            if os.path.exists(outname): os.remove(outname)
+            hdulist['dx',chip].data = fdx
+            hdulist['dy',chip].data = fdy
+            hdulist.writeto(outname)
+            outname = soutroot+'_sci'+str(chip)+'_newsub_dxy.fits'
+            if os.path.exists(outname): os.remove(outname)
+            hdulist['dx',chip].data = sdx
+            hdulist['dy',chip].data = sdy
+            hdulist.writeto(outname)
+
+            """
+            outname = outroot+'_sci'+str(chip)+'_diff_dxy.fits'
+            if os.path.exists(outname): os.remove(outname)
+            hdulist['dx',chip].data = diffx
+            hdulist['dy',chip].data = diffy
+            hdulist.writeto(outname)
+            """
+            print 'Created output file with differences named: ',outname
+    if output:
+        hdulist.close()
