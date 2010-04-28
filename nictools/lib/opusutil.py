a94011f5ced5c9d33d9e0116545cd48c2d43e85f
sienkiew <sienkiew@stsci.edu>
2010-04-28 13:57:03 +0000

    future imports at beginning of file
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@9251 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: 8d94dec3c0b4347bb44b4cf6a1b5ea3649c57eda

--- nictools/lib/opusutil.py
+++ nictools/lib/opusutil.py
@@ -7,11 +7,11 @@
 # 01/20/05  52348  Sherbert Remove "MSG_DEGUG" to obviate its use
 # 12/15/05  54226  Sontag   Added UsingLvl() and __all_valid_levels
 #---------------------------------------------------------------------------
+from __future__ import division  # confidence high
 import os
 import sys
 import string
 import time
-from __future__ import division  # confidence high
 
 # Read the environment setting for the PrintMsg report level
 # or default if not defined
