spacetelescope/stsci.stimage

commit 447d0462241796aa276f67af716d1630ffe57091

Problems with how the refimage and user-specified WCS parameters were applied to create the final output WCS were resolved in betadrizzle.wcs_functions.  Potential conflicts between the user-specified WCS for the single drizzle step and the output WCS for the final drizzle step were also cleaned up. This allowed the betadrizzle results to exactly match the coordinate transformation results reported by WTRAXY (and betadrizzle.wcs_functions.WCSMap). WJH
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@11486 fe389314-cf27-0410-b35b-8c050e845b92

Former-commit-id: 40efd5a551aa7e15bb7e6289bcec013a2135b09c

betadrizzle/lib/wcs_functions.py
@@ -441,16 +441,21 @@ def make_outputwcs(imageObjectList,output,configObj=None):
         keyname = 'driz_sep_'
         for key in singleParDict:
             k = key[len(keyname):]
-            single_pars[WCS_USERPARS[k]] = singleParDict[key]
+            if k != 'refimage':
+                single_pars[k] = singleParDict[key]
         #single_pars.update(singleParDict)
         
         # Now, account for any user-specified reference image
         if singleParDict[keyname+'refimage']:
+            def_wcs = default_wcs.deepcopy()
             default_wcs = wcsutil.HSTWCS(singleParDict[keyname+'refimage'])
 
         ### Create single_wcs instance based on user parameters
         outwcs.single_wcs = mergeWCS(default_wcs,single_pars)
-            
+        # restore global default WCS to original value so single_drizzle WCS does not
+        # influence final_drizzle WCS
+        default_wcs = def_wcs.deepcopy()
+        
     final_step = configObj[util.getSectionName(configObj,7)]
     finalParDict = configObj[util.getSectionName(configObj,'7a')].copy()
     if final_step['driz_combine'] and finalParDict['final_wcs']:         
@@ -458,8 +463,9 @@ def make_outputwcs(imageObjectList,output,configObj=None):
         keyname = 'final_'
         for key in finalParDict:
             k = key[len(keyname):]
-            final_pars[WCS_USERPARS[k]] = finalParDict[key]
-        final_pars.update(finalParDict)
+            if k != 'refimage':
+                final_pars[k] = finalParDict[key]
+        #final_pars.update(finalParDict)
         # Now, account for any user-specified reference image
         if finalParDict[keyname+'refimage']:
             default_wcs = wcsutil.HSTWCS(finalParDict[keyname+'refimage'])

betadrizzle/lib/__init__.py
@@ -64,7 +64,7 @@ try:
 except:
     __svn_version__ = 'Unable to determine SVN revision'
 
-__version__ = '4.0.6dev11453'
+__version__ = '4.0.6dev11485'
 # End Version Information ---------------------------------------------
 
 # Pointer to the included Python class for WCS-based coordinate transformations

betadrizzle/src/cdrizzlebox.c
@@ -711,7 +711,7 @@ do_kernel_gaussian(struct driz_param_t* p, const integer_t j,
         ddx = xx - (double)ii;
         /* Radial distance */
         r2 = ddx*ddx + ddy*ddy;
-
+        
         /* Weight is a scaled Gaussian function of radial
            distance */
         dover = p->gaussian.es * exp(-r2 * p->gaussian.efac);

commit b1eae25fe053b54ea9046c6e4b97897e7debc65d

This update to the 'wcs_functions' module in betadrizzle fixes a bug which caused an exception to be raised when user specified parameters were given for a custom single drizzle product. This clears up a logic problem introduced in r11486 for the single drizzle product.
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk@12516 fe389314-cf27-0410-b35b-8c050e845b92

Former-commit-id: 6205bde6626d9f27c4e10e025fb79241673cf490

betadrizzle/lib/wcs_functions.py
@@ -500,11 +500,10 @@ def make_outputwcs(imageObjectList,output,configObj=None):
             k = key[len(keyname):]
             if k != 'refimage':
                 single_pars[k] = singleParDict[key]
-        #single_pars.update(singleParDict)
         
         # Now, account for any user-specified reference image
+        def_wcs = default_wcs.deepcopy()
         if singleParDict[keyname+'refimage']:
-            def_wcs = default_wcs.deepcopy()
             default_wcs = wcsutil.HSTWCS(singleParDict[keyname+'refimage'])
 
         ### Create single_wcs instance based on user parameters
@@ -523,7 +522,6 @@ def make_outputwcs(imageObjectList,output,configObj=None):
             if k != 'refimage':
                 final_pars[k] = finalParDict[key]
 
-        #final_pars.update(finalParDict)
         # Now, account for any user-specified reference image
         if finalParDict[keyname+'refimage']:
             default_wcs = wcsutil.HSTWCS(finalParDict[keyname+'refimage'])