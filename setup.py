de5d9423e491e676004743671c66c4f97baabf14
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-29 12:07:45 -0400

    Implement RECON

--- setup.py
+++ setup.py
@@ -1,16 +1,59 @@
 #!/usr/bin/env python
+import recon.release
+from glob import glob
+from os import walk
+from os.path import abspath, curdir, join
+from numpy import get_include as np_include
+from setuptools import setup, find_packages, Extension
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+version = recon.release.get_info()
+recon.release.write_template(version, 'lib/stsci/stimage')
+
+sources = []
+for root, dirs, files in walk(curdir):
+    if 'test_c' in root:
+        continue
+    for f in files:
+        if f.endswith('.c'):
+            sources.append(join(abspath(root), f))
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils'],
-    namespace_packages=['stsci'], packages=['stsci'],
-    d2to1=True,
-    use_2to3=False,
-    zip_safe=False
+    name = 'stsci.stimage',
+    version = version.pep386,
+    author = 'Michael Droettboom',
+    author_email = 'help@stsci.edu',
+    description = 'Various image processing functions',
+    url = 'https://github.com/spacetelescope/stsci.stimage',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'nose',
+        'numpy',
+        'sphinx',
+        'stsci.sphinxext'
+    ],
+    package_dir = {
+        '': 'lib'
+    },
+    packages = find_packages(),
+    package_data = {
+        '': ['LICENSE'],
+    },
+    ext_modules=[
+        Extension('stsci.stimage._stimage',
+            sources,
+            include_dirs=[
+                np_include(),
+                'include',
+                'src_wrap',
+            ],
+        ),
+    ],
 )
