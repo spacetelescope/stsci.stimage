bbb62217c6c3ae8deda93082a74be121d74262c9
Joseph Hunkeler <jhunkeler@gmail.com>
2025-03-05 11:12:58 -0500

    Add test data generator

---
+++ test_c/generate_test_data.c
@@ -0,0 +1,68 @@
+#include <math.h>
+#include "test.h"
+
+#define fname_max 1024
+#define fname_default "numbers.txt"
+
+char *get_basename(char *s) {
+    const char *dirseps = "/\\";
+    size_t len = strlen(s);
+    do {
+       for (size_t i = 0; i < strlen(dirseps); i++) {
+           char ch = s[len];
+           char sep = dirseps[i];
+           char *tmp = &s[len];
+           if (ch == sep) {
+               return &tmp[len + strlen(tmp) ? 1 : 0];
+           }
+       }
+    } while (len--);
+    return s;
+}
+
+void usage(char *argv0) {
+    char *name = get_basename(argv0);
+    printf("usage: %s {nelem} [filename]\n", name);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Number of elements required\n");
+        usage(argv[0]);
+        exit(1);
+    }
+
+    char *nelem_ptr = NULL;
+    size_t nelem = strtoul(argv[1], &nelem_ptr, 10);
+    if (nelem_ptr && !nelem) {
+        fprintf(stderr, "Invalid number of elements: '%s'\n", nelem_ptr);
+        usage(argv[0]);
+        exit(1);
+    }
+
+    char outfile[fname_max] = {0};
+    if (argc < 3) {
+        strcpy(outfile, fname_default);
+    } else {
+        strcpy(outfile, argv[2]);
+    }
+
+    FILE *fp = fopen(outfile, "w+");
+    if (!fp) {
+        perror(outfile);
+        exit(1);
+    }
+
+    printf("Writing %zu elements to %s\n", nelem, outfile);
+    srand48(0);
+    for (size_t i = 0; i < nelem; i++) {
+        double v;
+        do {
+            v = drand48();
+        } while (!isfinite(v));
+        fprintf(fp, TEST_DATA_FMT "\n", v);
+    }
+
+    fclose(fp);
+    return 0;
+}
--- test_c/test.h
+++ test_c/test.h
@@ -1,22 +1,71 @@
 #ifndef STSCI_STIMAGE_TEST_H
 #define STSCI_STIMAGE_TEST_H
 
-#ifdef _WIN32
-
-#ifdef _MSC_VER
-
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+#include <float.h>
 
-#ifndef srand48
-#define srand48 srand
-#endif // srand48
+#define TEST_DATA_FMT "%lf"
 
-#ifndef drand48
-#define drand48() rand() / (RAND_MAX + 1.0)
-#endif // drand48
+extern inline const char *get_test_data_dir() {
+    const char *datadir = getenv("STIMAGE_TEST_DATA");
+    if (!datadir) {
+        fprintf(stderr, "error: STIMAGE_TEST_DATA environment variable must be set\n");
+        exit(1);
+    }
+    return datadir;
+}
 
-#endif // _MSC_VER
+extern inline double iter_test_data(double **data) {
+    double result = **data;
+    (*data)++;
+    return result;
+}
 
-#endif // _WIN32
+extern inline void get_test_data(const char *filename, double **result, size_t nelem) {
+    const char *datadir = get_test_data_dir();
+    char *path = NULL;
+    if (asprintf(&path, "%s/%s", datadir, filename) < 2) {
+        perror("path string creation failed");
+        exit(1);
+    }
+
+    *result = malloc(nelem * sizeof(**result));
+    if (!*result) {
+        perror("unable to allocate bytes for result array");
+        exit(1);
+    }
+
+    FILE *fp = fopen(path, "r");
+    if (!fp) {
+        perror(path);
+        exit(1);
+    }
+
+    char value_buf[255] = {0};
+    size_t i = 0;
+    while (i < nelem && fgets(value_buf, sizeof(value_buf) - 1, fp) != NULL) {
+        // truncate line feeds
+        value_buf[strcspn(value_buf, "\r\n")] = 0;
+
+        char *value_ptr = NULL;
+        double value = strtod(value_buf, &value_ptr);
+        if (!value_ptr && !value) {
+            fprintf(stderr, "invalid value in %s: '%s'\n", path, value_buf);
+            fclose(fp);
+            free(path);
+            exit(1);
+        }
+
+        *(*result + i) = value;
+        i++;
+    }
+
+    fclose(fp);
+    free(path);
+}
 
 #endif // STSCI_STIMAGE_TEST_H
