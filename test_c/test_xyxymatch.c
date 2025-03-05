c06dd374d74aaaf65998ed43ef169d8efc698df7
Joseph Hunkeler <jhunkeler@gmail.com>
2025-03-05 11:19:40 -0500

    use static dataset

--- test_c/test_xyxymatch.c
+++ test_c/test_xyxymatch.c
@@ -25,11 +25,12 @@ int main(int argc, char** argv) {
 
     stimage_error_init(&error);
 
-    srand48(0);
+    double *test_data_1 = NULL;
+    get_test_data("drand48_linux_1", &test_data_1, ncoords);
 
     for (i = 0; i < ncoords; ++i) {
-        ref[i].x = input[i].x = drand48();
-        ref[i].y = input[i].y = drand48();
+        ref[i].x = input[i].x = iter_test_data(&test_data_1);
+        ref[i].y = input[i].y = iter_test_data(&test_data_1);
     }
 
     status = xyxymatch(ncoords, input,
@@ -46,6 +47,7 @@ int main(int argc, char** argv) {
     }
 
     if (noutput != ncoords) {
+        printf("noutput must be equal to ncoords. noutput=%zu, ncoords=%d\n", noutput, ncoords);
         return 1;
     }
 
@@ -71,10 +73,10 @@ int main(int argc, char** argv) {
     /* Now with different values in input and ref */
 
     for (i = 0; i < ncoords; ++i) {
-        input[i].x = drand48();
-        input[i].y = drand48();
-        ref[i].x = drand48();
-        ref[i].y = drand48();
+        input[i].x = iter_test_data(&test_data_1);
+        input[i].y = iter_test_data(&test_data_1);
+        ref[i].x =   iter_test_data(&test_data_1);
+        ref[i].y =   iter_test_data(&test_data_1);
     }
 
     status = xyxymatch(ncoords, input,
@@ -91,6 +93,7 @@ int main(int argc, char** argv) {
     }
 
     if (noutput == 0 || noutput == ncoords) {
+        printf("noutput cannot be 0 or %d (noutput=%zu)\n", ncoords, noutput);
         return 1;
     }
 
